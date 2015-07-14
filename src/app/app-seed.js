890e53286dcbcf0b451f20619e43be0b2ab0b15f
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-07-14 11:48:44 -0400

    feat(app-seed): bootstrap app instances separately

---
+++ src/app/app-seed.js
@@ -0,0 +1,16 @@
+angular.element(document).ready(function () {
+    // convert html collection to array: http://blog.cluster-text.com/2013/04/29/a-trap-when-looping-on-getelementsbyclassname/
+    var nodes = [].slice.call(document.getElementsByClassName('fgpv')); 
+    var i;
+
+    nodes.forEach(function (node) {
+        // load shell template into the node
+        node.setAttribute('data-ng-include', '\'app/layout/shell.html\'');
+
+        // bootstrap each node as an Angular app
+        angular.bootstrap(node, ['app'], {
+            strictDi: true,
+        });
+    });
+
+});
---
+++ src/app/layout/shell.controller.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app.layout')
+        .controller('Shell', ['$scope', function ($scope) {
+            $scope.title = "Button title";
+
+            console.log($scope);
+        }]);
+
+})();
