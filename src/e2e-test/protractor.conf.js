114b1892028377d34dac2d8bdb4f9f0db7352e02
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-07-31 10:01:04 -0400

    chore(tests): setting up Protractor tests
    To run tests right now, run this in three different terminal windows:
     1. webdriver-manager start
     2. gulp serve:dev --protractor
     3. protractor src/e2e-test/protractor.conf.js

--- src/e2e-test/protractor.conf.js
+++ src/e2e-test/protractor.conf.js
@@ -1,5 +1,12 @@
 exports.config = {
+    // if you need this, make sure that your first ng-inlcude div is on the descendant of
+    // the roomElement; it doesn't work otherwise for some reason
+    //rootElement: 'div#guts',
+
     allScriptsTimeout: 11000,
+    getPageTimeout: 10000,
+
+    seleniumAddress: 'http://localhost:4444/wd/hub',
 
     specs: [
       '*.js'
@@ -9,7 +16,7 @@ exports.config = {
         'browserName': 'chrome'
     },
 
-    baseUrl: 'http://localhost:8000/app/',
+    baseUrl: 'http://localhost:6001/src/',
 
     framework: 'jasmine',
 
