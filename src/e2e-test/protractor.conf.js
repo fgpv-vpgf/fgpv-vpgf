da537189b8289de157b91ba9617af344f29ca6d8
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-07-31 11:39:29 -0400

    chore(tests): run e2e tests on Travis
    Cannot use Chrome dirver for some reason, use Firefox instead
    https://code.google.com/p/chromedriver/issues/detail?id=928
    https://code.google.com/p/chromedriver/issues/detail?id=736

--- src/e2e-test/protractor.conf.js
+++ src/e2e-test/protractor.conf.js
@@ -6,7 +6,7 @@ exports.config = {
     allScriptsTimeout: 11000,
     getPageTimeout: 10000,
 
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+    seleniumAddress: ' http://127.0.0.1:4444/wd/hub',
 
     specs: [
       '*.js'
