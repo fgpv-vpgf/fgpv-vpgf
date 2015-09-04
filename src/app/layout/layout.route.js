6aba4977e10e2ac1506c1860c8200d597650aae9
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-09-04 12:41:15 -0400

    feat(layout): add initial root app state

---
+++ src/app/layout/layout.route.js
@@ -0,0 +1,55 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.layout')
+        .run(layoutConfig);
+
+    layoutConfig.$inject = ['statehelper', '$state'];
+
+    /* @ngInject */
+    function layoutConfig(statehelper, $state) {
+        statehelper.configureStates(getStates());
+
+        // when layout is ready, go to the default state
+        $state.go('app');
+        //$state.go('app', {location: true}); // will change the url;
+        // need to detect if it's a single-page app or multiple apps
+    }
+
+    function getStates() {
+        return [
+            {
+                name: 'app',
+                config: {
+                    url: '/',
+                    views: {
+                        toolbarPlug: {
+                            template: '<div>main toolbar placeholder</div>'
+                        },
+                        panelPlug: {
+                            template: '<div>panel placeholder</div>'
+                        },
+                        detailsPlug: {
+                            template: '<div>details panel placeholder</div>'
+                        },
+                        geoSearchPlug: {
+                            template: '<div>geosearch placeholder</div>'
+                        }
+                    }
+                }
+            },
+            {
+                name: 'app.details',
+                config: {
+                    url: 'details',
+                    views: {
+                        'detailsPlug@': {
+                            template: '<div>details panel with content</div>'
+                        }
+                    }
+                }
+            }
+        ];
+    }
+})();
