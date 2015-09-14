578cbca34357b3e360bbef230890bf43203878ae
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-09-14 13:56:06 -0400

    feat(ui): add sidenav stub
    Animation is presently broken: https://github.com/angular/material/issues/4626

--- src/app/layout/shell.controller.js
+++ src/app/layout/shell.controller.js
@@ -5,13 +5,14 @@
         .module('app.layout')
         .controller('ShellController', ShellController);
 
-    ShellController.$inject = ['layoutConfig'];
+    ShellController.$inject = ['layoutConfig', 'rvSideNavigationService'];
 
-    function ShellController(layoutConfig) {
+    function ShellController(layoutConfig, rvSideNavigationService) {
         var vm = this;
 
         vm.config = layoutConfig;
         vm.isLoading = true;
+        vm.rvSideNavigationService = rvSideNavigationService;
 
         activate();
 
