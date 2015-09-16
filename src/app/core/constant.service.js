c48a504fc9830a33ea6d99645644e7d0d1f684cd
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-09-16 14:36:32 -0400

    feat(ui): add main toolbar initial code

--- src/app/core/constant.service.js
+++ src/app/core/constant.service.js
@@ -5,5 +5,8 @@
         .module('app.core')
         .constant('configDefaults', {
             title: 'Dawn RAM'
+        })
+        .constant('templateRegistry', {
+            toolbar: 'app/ui/toolbar/toolbar.html'
         });
 })();
