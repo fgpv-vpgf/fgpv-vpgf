98aa97ac4545068c4329b61c0e7724f5d2426c64
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-09-28 10:13:25 -0400

    feat(ui): stagger animation of the main panel
    If the side panel is open.

--- src/app/layout/main-panel-plug.controller.js
+++ src/app/layout/main-panel-plug.controller.js
@@ -6,12 +6,22 @@
         .controller('MainPanelPlugController', MainPanelPlugController);
 
     /* @ngInject */
-    function MainPanelPlugController() {
+    function MainPanelPlugController($rootScope) {
         var self = this;
         self.active = active;
+        self.isStaggering = false;
+
+        //////////////
 
         function active() {
             return true;
         }
+
+        // staggers the main panel's transition if the side panel is open
+        $rootScope.$on('$stateChangeStart',
+            function (event, toState) {
+                var sideReg = /(app)\.(main)\.(.*)\.(side)/;
+                self.isStaggering = sideReg.test(toState.name);
+            });
     }
 })();
