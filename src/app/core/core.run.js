16316f737d46d86fc15e9a6b0e91bf6a44612814
Aly Merchant <aly.merchant@ec.gc.ca>
2015-10-19 18:09:25 -0400

    feat(core): call geoapi init from the viewer init

--- src/app/core/core.run.js
+++ src/app/core/core.run.js
@@ -1,3 +1,4 @@
+/* global geoapi */
 (function () {
     'use strict';
 
@@ -13,19 +14,25 @@
         .module('app.core')
         .run(runBlock);
 
-    /* @ngInject */
-    function runBlock(configService, $rootScope, $translate) {
-        // wait until config is retrieved
-        configService.initialize()
-            .then(function () {
+    function runBlock(configService, $rootScope, $translate, $q, events) {
+        const promises = [];
+        promises.push(geoapi('//ec.cloudapp.net/~aly/esri/dojo/dojo.js', window));
+        promises.push(configService.initialize());
+
+        // wait on the config and geoapi
+        $q.all(promises)
+            .then(() => {
                 // initialize other services, if any
                 console.log('Config initialized');
+                $rootScope.$broadcast(events.rvReady);
+            })
+            .catch((reason) => {
+                console.error('Everything broke');
+                console.error(reason);
             });
 
         // to prevent FOUC need to load translation files with config initialization if we know the language
-        $rootScope.$on('$translateLoadingSuccess', function (data) {
-            console.log(data);
-        });
+        $rootScope.$on('$translateLoadingSuccess', data => console.log(data));
 
         // TODO: write language detection routine
         $translate.use('fr-CA');
