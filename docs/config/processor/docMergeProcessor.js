6b96c4e63d7582ae639560bf17f165b3ba602d97
Jack <jack.wen@ec.gc.ca>
2015-10-21 16:58:16 -0400

    chore(style): resolve style issues from the result JSCS and JSHint
    resolve style issues for indent, spaces, eof line feed

--- docs/config/processor/docMergeProcessor.js
+++ docs/config/processor/docMergeProcessor.js
@@ -1,55 +1,51 @@
 var _ = require('lodash');
 
-module.exports = function docMergeProcessor(log) {
-	return {
-		$runAfter: ['computePathsProcessor'],
-		$runBefore: ['renderDocsProcessor'],
-		$process: function (docs) {
+module.exports = function docMergeProcessor () {
+    'use strict';
+    return {
+        $runAfter: ['computePathsProcessor'],
+        $runBefore: ['renderDocsProcessor'],
+        $process: function (docs) {
 
-			var modules = _(docs).filter({docType: 'module'}).value();
+            var modules = _(docs).filter({docType: 'module'}).value();
 
-			// servie docs
-			var componentGroups = _(docs).filter({docType: 'componentGroup'}).value();
+            // servie docs
+            var componentGroups = _(docs).filter({docType: 'componentGroup'}).value();
 
-			componentGroups.forEach( function(doc) {
-					// determine parent docs
-					var parentIdx = _.findIndex(modules, function(a) {
-						return a.name === doc.moduleName;
-					});
+            componentGroups.forEach(function (doc) {
+                    // determine parent docs
+                    var parentIdx = _.findIndex(modules, function (a) {
+                        return a.name === doc.moduleName;
+                    });
 
-					var parentDoc = modules[parentIdx];
+                    var parentDoc = modules[parentIdx];
 
-					if (! parentDoc.hasOwnProperty('groups')) {
-							// add methods
-							parentDoc.groups = [];
-						}
+                    if (!parentDoc.hasOwnProperty('groups')) {
+                        // add methods
+                        parentDoc.groups = [];
+                    }
 
-					parentDoc.groups.push({
-						"title": doc.groupType,
-						"children":doc.components
-						}
-					);
+                    parentDoc.groups.push({
+                        title: doc.groupType,
+                        children:doc.components
+                    });
 
+                    // switch (doc.groupType){
+                    //  case "service":
+                    //  parentDoc["service"] = doc;
+                    //  break;
+                    //  case "function":
+                    //  parentDoc["function"] = doc;
+                    //  break;
+                    //  case "directive":
+                    //  parentDoc["directive"] = doc;
+                    //  break;
+                    //  default:
+                    //  log.info("Unkown group type detected:" + doc.groupType);
+                    // }
+                });
 
-					// switch (doc.groupType){
-					// 	case "service":
-					// 	parentDoc["service"] = doc;
-					// 	break;
-					// 	case "function":
-					// 	parentDoc["function"] = doc;
-					// 	break;
-					// 	case "directive":
-					// 	parentDoc["directive"] = doc;
-					// 	break;
-					// 	default:
-					// 	log.info("Unkown group type detected:" + doc.groupType);
-					// }
-				});
-
-
-			return _(docs).reject({docType: 'componentGroup'}).value();
-			// return docs;
-			
-		}
-	};
+            return _(docs).reject({docType: 'componentGroup'}).value();
+        }
+    };
 };
