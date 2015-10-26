c04dc2b920f05b2204caae817d8e6d4fe7ab9e0c
Aly Merchant <aly.merchant@ec.gc.ca>
2015-10-26 14:46:51 -0400

    docs(core): explain the startup process

--- src/index.js
+++ src/index.js
@@ -40,6 +40,10 @@ module.exports = function (esriLoaderUrl, window) {
 
     function makeDojoRequests() {
         return new Promise(function (resolve, reject) {
+
+            // NOTE: do not change the callback to an arrow function since we don't know if
+            // Dojo's require has any expectations of the scope within that function or
+            // does any odd metaprogramming
             window.require(esriDeps.map(deps => deps[0]), function () {
                 const esriBundle = {};
 
@@ -54,6 +58,12 @@ module.exports = function (esriLoaderUrl, window) {
         });
     }
 
+    // the startup for this module is:
+    // 1. add a script tag to load the API (this typically points to a custom ESRI build)
+    // 2. load all the ESRI and Dojo dependencies `makeDojoRequests()`
+    // 3. initialize all of our modules
+    // everything is done in an async model and the result is a promise which resolves to
+    // a reference to our API
     return new Promise(function (resolve, reject) {
         const oScript = window.document.createElement('script');
         const oHead = window.document.head || window.document.getElementsByTagName('head')[0];
