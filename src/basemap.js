513c71372deb3f42525d698e2c67827226e52c76
Jack <jack.wen@ec.gc.ca>
2015-11-24 15:45:12 -0500

    feat(core): add basemap submodule to geoApi
    add basemap.js to include basemap related submodule names with the associated esriBundle names
    bundle names will be defined in the index.js esriDeps

---
+++ src/basemap.js
@@ -0,0 +1,9 @@
+// Basemap related modules
+module.exports = function (esriBundle) {
+    return {
+        Basemap: esriBundle.Basemap,
+        BasemapGallery: esriBundle.BasemapGallery,
+        BasemapLayer: esriBundle.BasemapLayer,
+        BasemapToggle: esriBundle.BasemapToggle
+    };
+};
