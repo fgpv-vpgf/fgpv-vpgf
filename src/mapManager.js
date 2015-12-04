bdbd0d1a5ea92544f99ca2d4a47809f82ab6549b
Jack <Jack.Wen@ec.gc.ca>
2015-12-03 23:51:28 -0500

    feat(mapManager): return object containing individual initialized control/widget
    return object containing the setup BasemapControl.
    add detailed comments.
    add place holder for other map controls.

--- src/mapManager.js
+++ src/mapManager.js
@@ -1,17 +1,58 @@
 'use strict';
 var basemap = require('./basemap.js');
 
+// mapManager module, provides function to setup a map
 module.exports = function (esriBundle) {
     var mapManager = {};
 
+    /**
+    * setup map features with info provided by configuration
+    * @param {esriMap} map ESRI map object
+    * @param {Object} settings JSON object of map configurations
+    * @return {Object} object with reference to widgets/controls created following configuration
+    */
     mapManager.setupMap = function (map, settings) {
 
         // var map = arguments[0];
         // var settings = arguments[1];
+        var basemapCtrl;
 
-        var lbasemap = basemap(esriBundle);
+        // check to see if property exists in settings
+        if ('basemaps' in settings) {
 
-        return lbasemap.makeBasemaps(settings.basemaps, map, 'anchorId');
+            // need to pass esriBundle to basemap module in order to use it
+            // the alternative is to pass geoApi reference after creation, and then use the geoApi to
+            // access the properly initialized modules.
+            // or Is there an other way to do it?
+            let lbasemap = basemap(esriBundle);
+
+            // basemapCtrl is a basemap gallery object, should store this value for application use
+            // TODO: return as an part of JSON object?
+            basemapCtrl = lbasemap.makeBasemaps(settings.basemaps, map, 'anchorId');
+
+            // test the basemap by setting to map uid of 'baseEsriStreet', should remove after
+            // the obj is stored or returned as object
+            // TODO: remove the hard coded setting, demo purpose only
+            basemapCtrl.setBasemap('baseEsriStreet');
+        }
+
+        // TODO: add code to setup scalebar
+        if ('scalebar' in settings) {
+            console.log('scalebar setting exists');
+        } else {
+            console.log('scalebar setting does not exists');
+        }
+
+        // TODO: add code to setup north arrow
+
+        // TODO: add code to setup overview map
+
+        // TODO: add code to setup mouse co-ordinates
+
+        // return as object so we can use this in our geo section of fgpv
+        return {
+            BasemapControl: basemapCtrl
+        };
     };
 
     return mapManager;
