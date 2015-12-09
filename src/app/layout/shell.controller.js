0e81507aaeb0b5bf002b5ca5b21d601df950c174
Spencer Wahl <spencer.wahl@ec.gc.ca>
2015-12-09 13:43:25 -0500

    refactor(layout): remove stateHelper dependency

--- src/app/layout/shell.controller.js
+++ src/app/layout/shell.controller.js
@@ -14,7 +14,7 @@
         .module('app.layout')
         .controller('ShellController', ShellController);
 
-    function ShellController(configService, layoutService, $rootScope, events) {
+    function ShellController(configService, $rootScope, events) {
         const self = this;
 
         self.config = configService.data;
