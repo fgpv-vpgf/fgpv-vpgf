a7d0b7f05dc7d1531bce8d28527523e45d09ceb4
Cynthia (Qingwei) Li <Qingwei.Li@canada.ca>
2015-12-11 11:22:04 -0500

    feat(core): remove useless function

--- src/esri.js
+++ src/esri.js
@@ -1,31 +1,24 @@
 'use strict';
-const GeometryService = require('esri/tasks/GeometryService');
-const ProjectParameters = require('esri/tasks/ProjectParameters');
 
-function esriService(extent, sr) {
-    const params = new ProjectParameters();
-    const gsvc = new GeometryService('http://sncr01wbingsdv1.ncr.int.ec.gc.ca/arcgis/rest/' +
-     'services/Utilities/Geometry/GeometryServer');
+function esriService(esriBundle, extent, sr) {
+    const params = new esriBundle.ProjectParameters();
+    const gsvc = new esriBundle.GeometryService('http://sncr01wbingsdv1.ncr.int.ec.gc.ca/' +
+     'arcgis/rest/services/Utilities/Geometry/GeometryServer');
 
     params.geometries = extent;
     params.outSR = sr;
 
     gsvc.project(params, function (projectedExtents) {
-        // return array returned from esriService
-        return projectedExtents;
-    });
-}
+        console.log(projectedExtents);
 
-function projectEsriExtentBuilder(esriBundle) {
-    return (extent, sr) => {
-        let projectedExtents = esriService(extent, sr);
-        return new esriBundle.esriExtent(projectedExtents);
-    };
+        // return array returned from esriService
+        return new esriBundle.EsriExtent(projectedExtents);
+    });
 }
 
 module.exports = function (esriBundle) {
     return {
-        localProjectExtent: esriService,
-        projectEsriExtent: projectEsriExtentBuilder(esriBundle)
+        esriBundle: esriBundle,
+        esriService: esriService
     };
 };
