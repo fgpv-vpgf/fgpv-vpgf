19c12d142cc87a8d014526e3d68df889107705bb
Cynthia (Qingwei) Li <Qingwei.Li@canada.ca>
2015-12-11 11:21:09 -0500

    test(core): add esri import to test

--- spec/esriSpec.js
+++ spec/esriSpec.js
@@ -1,33 +1,43 @@
 /* jshint jasmine: true */
 'use strict';
-var File = require('vinyl');
-var es = require('event-stream');
-var geoApi = require('../');
+const esri = require('../src/esri.js');
 
 describe("test for esri projection conversion function", function() {
-  var x;
-  beforeEach(function() {
-    // set up spy/fake grey map???
-    var geoApi = jasmine.createSpy("esriService");
-    x = 1;
-  })
+    /*const fakeEsri = {
+        ProjectParameters: index.ProjectParameters,
+        GeometryService: index.GeometryService
+    };*/
+    const sampleData = {x0:-95,y0:49,x1:-94.5,y1:49.5,sr:4326};
+    // const sampleExtent = makeFakeEsriExtent(sampleData);
+    let esri;
+    let x;
 
-  afterEach(function() {
-    // tear down spy/fake grey map???
-  })
+    function makeFakeEsriExtent(o) {
+        return {
+            "xmin":o.x0,"ymin":o.y0,"xmax":o.x1,"ymax":o.y1,
+            "spatialReference":{"wkid":o.sr}
+        };
+    }
 
-  it("should return a spatial reference with given input", function() {
-    // expect spatial reference returned not null
-    expect(x).toBe(1);
-  });
+    beforeEach(function() {
+    // reset esri
+        // index();
+        // esri = esri(fakeEsri);
+        x = 1;
+    })
 
-  it("spatial reference should be different from original", function() {
-    // find original sr from config/params or whatever then != new sr
-    expect(x).toBe(1);
-  });
+    it('should use the target WKID when reprojecting', function() {
+        // let res = esri.esriService(fakeEsri, sampleExtent, 3978);
+        // expect(res.sr).toEqual(3978);
+    });
 
-  it("geometry service should be instantiated given correct params", function() {
-    // geometryService != null assert that after instantiating it.
-    expect(x).toBe(1);
-  });
+    it("spatial reference should be different from original", function() {
+        // find original sr from config/params or whatever then != new sr
+        expect(x).toBe(1);
+    });
+
+    it("geometry service should be instantiated given correct params", function() {
+        // geometryService != null assert that after instantiating it.
+        expect(x).toBe(1);
+    });
 });
