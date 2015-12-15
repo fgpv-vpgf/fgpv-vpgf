44aaf98de187eb3efb2d5e40c9416d972acca527
Spencer Wahl <spencer.wahl@ec.gc.ca>
2015-12-15 11:32:23 -0500

    chore(build): use gulp plugin loader
    Also changed en and fr to en-CA, fr-CA respectively.

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,6 @@ var bowerFiles = require('main-bower-files');
 var Dgeni = require('dgeni');
 var config = require('./gulp.config')();
 var Server = require('karma').Server;
-var gulpi18nCsv = require('gulp-i18n-csv');
 
 require('gulp-help')(gulp);
 
@@ -169,7 +168,7 @@ gulp.task('assetcopy', 'Copy fixed assets to the build directory',
 gulp.task('translate', 'Split translation csv into internationalized files',
     function () {
         return gulp.src(config.src + 'locales/translations.csv')
-            .pipe(gulpi18nCsv())
+            .pipe($.i18nCsv())
             .pipe(gulp.dest(config.build));
     });
 
@@ -231,18 +230,22 @@ gulp.task('serve:dev', 'Build the application and start a local development serv
  * -- coverage  : generate test coverage info
  * @return {Stream}
  */
-gulp.task('test', 'Run style checks and unit tests', ['vet', 'templatecache', 'translate'], function (done) {
-    startTests(true, done);
-});
+gulp.task('test', 'Run style checks and unit tests',
+    ['vet', 'templatecache', 'translate'],
+    function (done) {
+        startTests(true, done);
+    });
 
 /**
  * Run specs and wait.
  * -- coverage  : generate test coverage info
  * Watch for file changes and re-run tests on each change
  */
-gulp.task('test:auto', 'Run unit tests and keep watching for changes', ['vet', 'templatecache', 'translate'], function (done) {
-    startTests(false, done);
-});
+gulp.task('test:auto', 'Run unit tests and keep watching for changes',
+    ['vet', 'templatecache', 'translate'],
+    function (done) {
+        startTests(false, done);
+    });
 
 /**
  * Generates the changelog from commit messages.
