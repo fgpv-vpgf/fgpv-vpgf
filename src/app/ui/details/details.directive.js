0f1d283a1505762a30c3a1efd9d2b766b6e33688
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-12-30 12:07:03 -0500

    feat(ui): add details panel content

--- src/app/ui/details/details.directive.js
+++ src/app/ui/details/details.directive.js
@@ -1,5 +1,9 @@
+/* global Ease, BezierEasing, TweenLite, HolderIpsum */
 (() => {
     'use strict';
+
+    const RV_SLIDE_DURATION = 0.3;
+    const RV_SWIFT_IN_OUT_EASE = new Ease(BezierEasing(0.35, 0, 0.25, 1));
 
     /**
      * @ngdoc directive
@@ -30,7 +34,43 @@
 
         ///////////
 
-        function link() { //scope, el, attr, ctrl) {
+        function link(scope, element) { //scope, element, attr, ctrl) {
+            const self = scope.self;
+            let section;
+            let layerList;
+
+            const tl = new TimelineLite({
+                paused: true
+            });
+
+            //element.on('.rv-details-layer-list', 'click', onEnter);
+            //element.on('.rv-details-layer-list', 'onmouseleave', onLeave);
+
+            self.onEnter = onEnter;
+            self.onLeave = onLeave;
+
+            function onEnter() {
+                if (!layerList) {
+                    // find layer node and construct timeline
+                    layerList = element.find('.rv-details-layer-list');
+                    section = element.find('.rv-details');
+
+                    tl.to(layerList, RV_SLIDE_DURATION, {
+                            width: 280,
+                            ease: RV_SWIFT_IN_OUT_EASE
+                        })
+                        .to(section, RV_SLIDE_DURATION, {
+                            className: '+=rv-expanded'
+                        }, 0);
+                }
+
+                console.log('hover');
+                tl.play();
+            }
+
+            function onLeave() {
+                tl.reverse()
+            }
 
         }
     }
@@ -38,18 +78,69 @@
     function Controller(stateManager) {
         'ngInject';
         const self = this;
+
         self.closeDetails = closeDetails;
+
+        self.selectLayer = selectLayer;
+
+        self.data = {
+            layers: [
+                {
+                    name: HolderIpsum.words(3, true),
+                    type: 'something',
+                    items: [
+                        {
+                            name: HolderIpsum.words(3, true),
+                            data: [HolderIpsum.sentence(), HolderIpsum.sentence(), HolderIpsum.sentence(),
+                                HolderIpsum.sentence()]
+                        },
+                        {
+                            name: HolderIpsum.words(3, true),
+                            data: [HolderIpsum.sentence(), HolderIpsum.sentence()]
+                        }
+                    ]
+                },
+                {
+                    name: HolderIpsum.words(3, true),
+                    type: 'something',
+                    items: [
+                        {
+                            name: HolderIpsum.words(3, true),
+                            data: [HolderIpsum.sentence(), HolderIpsum.sentence()]
+                        }
+                    ]
+                },
+                {
+                    name: HolderIpsum.words(3, true),
+                    type: 'something',
+                    items: [
+                        {
+                            name: HolderIpsum.words(3, true),
+                            data: [HolderIpsum.sentence(), HolderIpsum.sentence()]
+                        }
+                    ]
+                }
+            ]
+        };
 
         activate();
 
         ///////////
 
         function activate() {
-
+            self.selected = self.data.layers[0];
         }
 
         function closeDetails() {
-            stateManager.set({ side: false }, 'mainToc');
+            stateManager.set({
+                side: false
+            }, 'mainToc');
+        }
+
+        function selectLayer(layer) {
+            self.selected = layer;
+
+            self.onLeave();
         }
     }
 })();
