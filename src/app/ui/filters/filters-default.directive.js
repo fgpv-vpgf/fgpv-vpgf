1f35cab0e8491758634e263562f1dc37c8257b47
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2016-01-15 13:38:19 -0500

    feat(ui): create table on setting layer data
    Related #183

--- src/app/ui/filters/filters-default.directive.js
+++ src/app/ui/filters/filters-default.directive.js
@@ -36,19 +36,70 @@
          */
         function linkFunc(scope, el) { //scope, el, attr, ctrl) {
             const self = scope.self;
+            let containerNode;
+
+            self.createTable = createTable;
+
+            function createTable() {
+                if (!containerNode) {
+                    containerNode = el.find('.rv-filters-data-container');
+                }
+
+                if (self.table) {
+                    // destroy table with all events
+                    self.table.destroy(true); //https://datatables.net/reference/api/destroy()
+                    delete self.table; // kill the reference
+                }
+
+                let tableNode = angular.element('<table class="display nowrap rv-data-table"></table>');
+                containerNode.append(tableNode);
+
+                //let tableInitialized = false;
+                console.log(containerNode, tableNode);
+
+                //.clear()
+
+                self.table = tableNode
+                    .on('init.dt', () => {
+                        // wait for table to initialize fully: https://datatables.net/reference/event/init
+
+                        //scope.$digest(); // need to kick in the digest cycle since we are using non-Angular event here.
+                        //tableInitialized = true;
+
+                        stateManager.display.filters.isLoading = false;
+
+                        //self.draw();
+                        console.log('Table initialisation complete: ' + new Date()
+                            .getTime());
+                    })
+                    .DataTable({
+                        dom: 'rti',
+                        columns: stateManager.display.filters.data.columns,
+                        data: stateManager.display.filters.data.data,
+                        deferRender: true,
+                        scrollY: true,
+                        scroller: true
+                    });
+
+                //self.table = table;
+                /*if (tableInitialized) {
+                    self.draw();
+                    self.table.destroy(true);
+                }*/
+            }
 
             // FIX: generating datatble in the hidden node is a bit funky, so there is a delay to let us open the panel
             // to see the table;
             // obviously it shouldn't be happening in production.
             // TODO: write proper logic to generate the table while hidden and then redraw it on show
-            stateManager.state.filtersFulldata.display.isLoading = true;
+            //stateManager.state.filtersFulldata.display.isLoading = true;
 
-            $timeout(() => {
+            /*$timeout(() => {
                 let table = el.find('.rv-data-table')
                     .first()
                     .on('init.dt', function () {
                         // wait for table to initialize fully: https://datatables.net/reference/event/init
-                        stateManager.state.filtersFulldata.display.isLoading = false;
+                        //stateManager.state.filtersFulldata.display.isLoading = false;
                         scope.$digest(); // need to kick in the digest cycle since we are using non-Angular even here.
                         self.draw();
 
@@ -80,7 +131,7 @@
                     });
 
                 self.table = table;
-            }, 10000); // wait for ten seconds so we can open data panel
+            }, 10000); // wait for ten seconds so we can open data panel*/
         }
     }
 
@@ -91,7 +142,7 @@
         'ngInject';
         const self = this;
 
-        self.display = stateManager.state.filtersFulldata.display;
+        self.display = stateManager.display.filters;
 
         self.draw = draw;
 
@@ -100,15 +151,24 @@
         function activate() {
             // wait for morph on filters panel to complete and redraw the datatable
             $scope.$on('stateChangeComplete', (event, name, property, value, skip) => {
-                console.log(event, name, property, value, skip);
                 if (name === 'filters') {
+                    console.log(event, name, property, value, skip);
                     self.draw();
                 }
+            });
+
+            console.log(self.display);
+            $scope.$watch('self.display.data', newValue => {
+                if (newValue.data) {
+                    self.createTable();
+                }
+                console.log(newValue);
             });
         }
 
         // re draw the table using scroller extension
         function draw() {
+            console.log('drawing', self.table);
             if (self.table) {
                 self.table.scroller.measure();
             }
