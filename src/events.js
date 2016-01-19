27801dd8ece41aaf07684ebde75a44d987ab2bc0
Cynthia (Qingwei) Li <Qingwei.Li@ec.gc.ca>
2016-01-19 13:48:31 -0500

    feat(core): wrapEvents skeleton

---
+++ src/events.js
@@ -0,0 +1,18 @@
+/**
+ * Wire up any supplied handlers to the corresponding dojo .on events on layer.
+ * Purpose is to keep Dojo .on events contained in geoApi.
+ *
+ * @param {obj} object from which the events pertain to
+ * @param {layer} an ESRI layer object
+ * @param {handlers} an event handler definition object
+ */
+
+function wrapEvents(obj, layer, handlers) {
+    // use function getLayerType to determine layer type. (in shared.js)
+}
+
+module.exports = function (esriBundle) {
+    return {
+
+    };
+};
