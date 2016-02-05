b5627fcb1306de03b8d6c571c53babf5bc76ad39
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2016-02-05 10:54:42 -0500

    feat(core): add display link to panel state

--- src/app/common/router/statemanager.constant.service.js
+++ src/app/common/router/statemanager.constant.service.js
@@ -1,12 +1,13 @@
 (() => {
     'use strict';
 
-    const STATE_OBJECT_DEFAULTS = parentName => {
+    const STATE_OBJECT_DEFAULTS = (parentName, displayName) => {
         if (parentName) {
             return {
                 parent: parentName,
                 active: false,
-                activeSkip: false
+                activeSkip: false,
+                display: displayName
             };
         } else {
             return {
@@ -54,16 +55,17 @@
             // `morph` indicates the mode of the panel; filters panel has three different modes: 'full', 'default', and 'minimized'; filters panel's modes specify different height for the panel; its changes are also animated;
             // `morphSkip` is a boolean flag indicating whether the animation on changes to the `morph` should be skipped
             // `history` keeps track of pane names opened in a panel; limit of 10 items;
+            // 'displayName' links panel to a certain display object; several panels can target the same display object;
 
             main: STATE_OBJECT_DEFAULTS(),
             mainToc: STATE_OBJECT_DEFAULTS('main'),
             mainToolbox: STATE_OBJECT_DEFAULTS('main'),
-            mainDetails: STATE_OBJECT_DEFAULTS('main'),
+            mainDetails: STATE_OBJECT_DEFAULTS('main', 'details'),
             side: STATE_OBJECT_DEFAULTS(),
-            sideMetadata: STATE_OBJECT_DEFAULTS('side'),
-            sideSettings: STATE_OBJECT_DEFAULTS('side'),
+            sideMetadata: STATE_OBJECT_DEFAULTS('side', 'metadata'),
+            sideSettings: STATE_OBJECT_DEFAULTS('side', 'settings'),
             filters: STATE_OBJECT_DEFAULTS(),
-            filtersFulldata: STATE_OBJECT_DEFAULTS('filters'),
+            filtersFulldata: STATE_OBJECT_DEFAULTS('filters', 'filters'),
             filtersNamedata: STATE_OBJECT_DEFAULTS('filters'),
             other: STATE_OBJECT_DEFAULTS(),
             otherBasemap: STATE_OBJECT_DEFAULTS('other'),
