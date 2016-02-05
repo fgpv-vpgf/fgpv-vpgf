c78cb991f76f5a0c2e73f3d3b831984995b4505e
ec-dev <jack.wen@ec.gc.ca>
2016-02-05 18:05:46 -0500

    feat(geo): add re-projection code for full extent
    add code to check for the need for re-projection full extent

--- src/app/geo/geo.service.js
+++ src/app/geo/geo.service.js
@@ -329,15 +329,56 @@
 
             if (config.fullExtent) {
 
-                // full extent available in config
-                mapSettings.fullExtent = config.fullExtent;
-                fullExtent = service.gapi.mapManager.Extent(mapSettings.fullExtent);
+                service.gapi.events.wrapEvents(map, {
+                    load: () => {
+                        // full extent available in config
+                        mapSettings.fullExtent = config.fullExtent;
+
+                        // compare map extent and setting.extent spatial-references
+                        let mapExtent = map.extent;
+
+                        if (isSpatialRefEqual(mapExtent.spatialReference, mapSettings.fullExtent.spatialReference)) {
+
+                            // same spatial reference, no reprojection required
+                            fullExtent = service.gapi.mapManager.Extent(mapSettings.fullExtent);
+                        } else {
+
+                            // need to re-project
+                            fullExtent = service.gapi.proj.projectEsriExtent(
+                                service.gapi.mapManager.Extent(mapSettings.fullExtent),
+                                mapExtent.spatialReference);
+                        }
+                    }
+                });
+
             }
 
             mapManager = service.gapi.mapManager.setupMap(map, mapSettings);
 
             // FIXME temp link for debugging
             window.FGPV = { layers: service.layers };
+        }
+
+        /**
+        * Checks if two spatial reference objects are equivalent.  Handles both wkid and wkt definitions
+        *
+        * @method isSpatialRefEqual
+        * @static
+        * @param {type} sr1 Esri Spatial Reference First to compare
+        * @param {type} sr2 Esri Spatial Reference Second to compare
+        * @return {Boolean} true if the two spatial references are equivalent.  False otherwise.
+        */
+        function isSpatialRefEqual(sr1, sr2) {
+            if ((sr1.wkid) && (sr2.wkid)) {
+                //both SRs have wkids
+                return sr1.wkid === sr2.wkid;
+            } else if ((sr1.wkt) && (sr2.wkt)) {
+                //both SRs have wkt's
+                return sr1.wkt === sr2.wkt;
+            } else {
+                //not enough info provided or mismatch between wkid and wkt.
+                return false;
+            }
         }
 
         /**
