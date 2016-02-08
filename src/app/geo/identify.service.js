961a83540d37cacd199023341c5988dfa51a77da
Aly Merchant <aly.merchant@ec.gc.ca>
2016-02-08 13:14:00 -0500

    feat(geo): add click handler for dynamic layers

---
+++ src/app/geo/identify.service.js
@@ -0,0 +1,76 @@
+(() => {
+    /**
+     * @ngdoc service
+     * @name identifyService
+     * @module app.geo
+     *
+     * @description
+     * Generates handlers for feature identification on all layer types.
+     */
+    angular
+        .module('app.geo')
+        .factory('identifyService', identifyService);
+
+    function identifyService(stateManager) {
+
+        const dynamicLayers = [];
+
+        return (geoApi, map) => {
+            geoApi.events.wrapEvents(map, { click: clickHandlerBuilder(geoApi, map) });
+
+            return {
+                addDynamicLayer: (layer, name) => { dynamicLayers.push({ layer, name }); }
+            };
+        };
+
+        ////////
+
+        function clickHandlerBuilder(geoApi, map) {
+            return clickEvent => {
+                if (dynamicLayers.length === 0) { return; }
+
+                console.info('Click start');
+                const detailsPanel = stateManager.display.details;
+                detailsPanel.isLoading = true;
+                detailsPanel.data = [];
+
+                const opts = {
+                    geometry: clickEvent.mapPoint,
+                    width: map.width,
+                    height: map.height,
+                    mapExtent: map.extent,
+                };
+
+                const idPromises = dynamicLayers.map(({ layer, name }) => {
+                    const result = {
+                        isLoading: true,
+                        requestId: -1,
+                        requester: name,
+                        data: []
+                    };
+                    detailsPanel.data.push(result);
+                    return geoApi.layer.serverLayerIdentify(layer, opts)
+                        .then(clickResults => {
+                            console.log('got a click result');
+                            console.log(clickResults);
+                            result.data = [{ name, data: JSON.stringify(clickResults) }];
+                            result.isLoading = false;
+                        })
+                        .catch(err => {
+                            console.warn('Identify failed');
+                            console.warn(err);
+                            result.data = JSON.stringify(err);
+                            result.isLoading = false;
+                        });
+                });
+
+                Promise.all(idPromises)
+                    .then(() => { detailsPanel.isLoading = false; })
+                    .catch(() => { detailsPanel.isLoading = false; });
+
+                stateManager.setActive({ side: false }, 'mainDetails');
+            };
+        }
+
+    }
+})();
