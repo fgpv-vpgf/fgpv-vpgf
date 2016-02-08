ac798a8a9b6678a7d37d462fd776ae54139739a4
James Rae <james.rae@ec.gc.ca>
2016-02-08 14:12:29 -0500

    feat(ui): mapnav service i18n support

--- src/app/ui/mapnav/mapnav.service.js
+++ src/app/ui/mapnav/mapnav.service.js
@@ -19,7 +19,7 @@
      *
      * @return {object} service object
      */
-    function mapNavigationService(stateManager, geoService) {
+    function mapNavigationService(stateManager, geoService, $translate) {
         const service = {
             // FIXME: this config snippet should obvisouly come from config service
             config: {
@@ -38,48 +38,48 @@
         // navigation controls presets
         service.controls = {
             zoomIn: {
-                label: 'Zoom in',
+                label: '',
                 icon: 'content:add',
-                tooltip: 'Zoom in',
+                tooltip: '',
                 action: () => geoService.shiftZoom(1)
             },
             slider: {
                 // TODO: add slider properties when we find a suitable slider lib
             },
             zoomOut: {
-                label: 'Zoom out',
+                label: '',
                 icon: 'content:remove',
-                tooltip: 'Zoom out',
+                tooltip: '',
                 action: () => geoService.shiftZoom(-1)
             },
             geoLocation: {
-                label: 'Your Location',
+                label: '',
                 icon: 'maps:my_location',
-                tooltip: 'Your Location',
+                tooltip: '',
                 action: function () {} // FIXME: user proper call
             },
             marquee: {
-                label: '???',
+                label: '',
                 icon: 'action:search',
-                tooltip: '???',
+                tooltip: '',
                 action: function () {} // FIXME: user proper call
             },
             home: {
-                label: 'Canada',
+                label: '',
                 icon: 'action:home',
-                tooltip: 'Canada',
+                tooltip: '',
                 action: () => geoService.setFullExtent()
             },
             history: {
-                label: 'History',
+                label: '',
                 icon: 'action:history',
-                tooltip: 'History',
+                tooltip: '',
                 action: function () {} // FIXME: user proper call
             },
             basemap: {
-                label: 'Basemap',
+                label: '',
                 icon: 'maps:map',
-                tooltip: 'Basemap',
+                tooltip: '',
 
                 // TODO: revise how mode is detected
                 selected: () => stateManager.state.mapnav.morph !== 'default',
@@ -93,6 +93,29 @@
                 }
             }
         };
+
+        $translate.onReady(() => {
+            service.controls.zoomIn.tooltip = $translate.instant('nav.tooltip.zoomIn');
+            service.controls.zoomIn.label = $translate.instant('nav.label.zoomIn');
+
+            service.controls.zoomOut.tooltip = $translate.instant('nav.tooltip.zoomOut');
+            service.controls.zoomOut.label = $translate.instant('nav.label.zoomOut');
+
+            service.controls.geoLocation.tooltip = $translate.instant('nav.tooltip.geoLocation');
+            service.controls.geoLocation.label = $translate.instant('nav.label.geoLocation');
+
+            service.controls.marquee.tooltip = $translate.instant('nav.tooltip.search');
+            service.controls.marquee.label = $translate.instant('nav.label.search');
+
+            service.controls.home.tooltip = $translate.instant('nav.tooltip.home');
+            service.controls.home.label = $translate.instant('nav.label.home');
+
+            service.controls.history.tooltip = $translate.instant('nav.tooltip.history');
+            service.controls.history.label = $translate.instant('nav.label.history');
+
+            service.controls.basemap.tooltip = $translate.instant('nav.tooltip.basemap');
+            service.controls.basemap.label = $translate.instant('nav.label.basemap');
+        });
 
         return service;
 
