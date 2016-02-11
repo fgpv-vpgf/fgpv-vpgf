ed56a2b9efeea3a35f63ba647b3bdc9b46dc206f
James Rae <james.rae@ec.gc.ca>
2016-02-11 11:13:56 -0500

    refactor(ui): use filters for mapnav i18n

--- src/app/ui/mapnav/mapnav.service.js
+++ src/app/ui/mapnav/mapnav.service.js
@@ -19,7 +19,7 @@
      *
      * @return {object} service object
      */
-    function mapNavigationService(stateManager, geoService, $translate) {
+    function mapNavigationService(stateManager, geoService) {
         const service = {
             // FIXME: this config snippet should obvisouly come from config service
             config: {
@@ -38,48 +38,48 @@
         // navigation controls presets
         service.controls = {
             zoomIn: {
-                label: '',
+                label: 'nav.label.zoomIn',
                 icon: 'content:add',
-                tooltip: '',
+                tooltip: 'nav.tooltip.zoomIn',
                 action: () => geoService.shiftZoom(1)
             },
             slider: {
                 // TODO: add slider properties when we find a suitable slider lib
             },
             zoomOut: {
-                label: '',
+                label: 'nav.label.zoomOut',
                 icon: 'content:remove',
-                tooltip: '',
+                tooltip: 'nav.tooltip.zoomOut',
                 action: () => geoService.shiftZoom(-1)
             },
             geoLocation: {
-                label: '',
+                label: 'nav.label.geoLocation',
                 icon: 'maps:my_location',
-                tooltip: '',
+                tooltip: 'nav.tooltip.geoLocation',
                 action: function () {} // FIXME: user proper call
             },
             marquee: {
-                label: '',
+                label: 'nav.label.search',
                 icon: 'action:search',
-                tooltip: '',
+                tooltip: 'nav.tooltip.search',
                 action: function () {} // FIXME: user proper call
             },
             home: {
-                label: '',
+                label: 'nav.label.home',
                 icon: 'action:home',
-                tooltip: '',
+                tooltip: 'nav.tooltip.home',
                 action: () => geoService.setFullExtent()
             },
             history: {
-                label: '',
+                label: 'nav.label.history',
                 icon: 'action:history',
-                tooltip: '',
+                tooltip: 'nav.tooltip.history',
                 action: function () {} // FIXME: user proper call
             },
             basemap: {
-                label: '',
+                label: 'nav.label.basemap',
                 icon: 'maps:map',
-                tooltip: '',
+                tooltip: 'nav.tooltip.basemap',
 
                 // TODO: revise how mode is detected
                 selected: () => stateManager.state.mapnav.morph !== 'default',
@@ -94,28 +94,17 @@
             }
         };
 
+        //TODO when time permits, investigate this alternate form of translation.
+        //     we are currently using filters; performance testing should be done
+        //     to see if they have a significant impact compared to this approach
+        //     see https://github.com/fgpv-vpgf/fgpv-vpgf/commit/ac798a8a9b6678a7d37d462fd776ae54139739a4
+        /*
         $translate.onReady(() => {
             service.controls.zoomIn.tooltip = $translate.instant('nav.tooltip.zoomIn');
             service.controls.zoomIn.label = $translate.instant('nav.label.zoomIn');
-
-            service.controls.zoomOut.tooltip = $translate.instant('nav.tooltip.zoomOut');
-            service.controls.zoomOut.label = $translate.instant('nav.label.zoomOut');
-
-            service.controls.geoLocation.tooltip = $translate.instant('nav.tooltip.geoLocation');
-            service.controls.geoLocation.label = $translate.instant('nav.label.geoLocation');
-
-            service.controls.marquee.tooltip = $translate.instant('nav.tooltip.search');
-            service.controls.marquee.label = $translate.instant('nav.label.search');
-
-            service.controls.home.tooltip = $translate.instant('nav.tooltip.home');
-            service.controls.home.label = $translate.instant('nav.label.home');
-
-            service.controls.history.tooltip = $translate.instant('nav.tooltip.history');
-            service.controls.history.label = $translate.instant('nav.label.history');
-
-            service.controls.basemap.tooltip = $translate.instant('nav.tooltip.basemap');
-            service.controls.basemap.label = $translate.instant('nav.label.basemap');
+            ...
         });
+        */
 
         return service;
 
