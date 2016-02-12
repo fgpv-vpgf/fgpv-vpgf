af354e5b8f4b389c9c4e0c0f65c7b23c451d1fba
ec-dev <jack.wen@ec.gc.ca>
2016-02-11 19:17:09 -0500

    fix(geo): rename variable missed from previous refactor

--- src/app/geo/geo.service.js
+++ src/app/geo/geo.service.js
@@ -409,7 +409,7 @@
                             // need to re-project
                             fullExtent = service.gapi.proj.projectEsriExtent(
                                 service.gapi.mapManager.getExtentFromSetting(mapSettings.fullExtent),
-                                mapExtent.spatialReference);
+                                map.extent.spatialReference);
                         }
                     }
                 });
