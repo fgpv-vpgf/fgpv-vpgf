dc619a5ac77f2a816ca1e9af2f63728ec7e696c8
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2016-02-24 09:00:44 -0500

    refactor(:doughnut:): turn shell into a directive

--- src/app/app-seed.js
+++ src/app/app-seed.js
@@ -12,7 +12,7 @@ angular.element(document)
             // we need to create an explicit child under app's root node, otherwise animation
             // doesnt' work; see this plunk: http://plnkr.co/edit/7EIM71IOwC8h1HdguIdD
             // or this one: http://plnkr.co/edit/Ds8e8d?p=preview
-            child = angular.element('<div>').attr('ng-include', '\'app/layout/shell.html\'')[0];
+            child = angular.element('<rv-shell>')[0];
             node.appendChild(child);
 
             // bootstrap each node as an Angular app
