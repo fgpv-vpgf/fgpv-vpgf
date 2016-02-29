80bacd6f3b12adf935a17aae5afe205c842e196f
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2016-02-29 12:42:08 -0500

    feat(:pouting_cat:): add error message to upload file step

--- src/app/ui/loader/loader-file.directive.js
+++ src/app/ui/loader/loader-file.directive.js
@@ -35,9 +35,11 @@
         }
     }
 
-    function Controller($timeout, stepper) {
+    function Controller($scope, $timeout, stepper) {
         'ngInject';
         const self = this;
+
+        // self.__form1 = {a: 999};
 
         self.dropActive = false; // flag to indicate if file drop zone is active
 
@@ -48,25 +50,44 @@
             csv: 'CSV',
             shapefile: 'Shapefile'
         };
+        self.fields = {
+            one: 'one',
+            two: 'two',
+            three: 'three'
+        };
 
         self.file = null; // flow file object
         self.dataType = null; // string
 
-        self.filesSubmitted = filesSubmitted; // a file is selected or dropped
+        /*self.filesSubmitted = filesSubmitted; // a file is selected or dropped
         self.fileSuccess = fileSuccess;
-        self.fileError = fileError;
+        self.fileError = fileError;*/
 
         activate();
 
         /*********/
 
         function activate() {
+
+            self.upload = {
+                step: {
+                    titleValue: 'Upload data',
+                    stepNumber: 1,
+                    isActive: false,
+                    isCompleted: false
+                },
+                form: null,
+                file: null,
+                fileSelectorOpened, // wanted to use es6 lexical this, but jshint complains; need to update to the newer version of jshint
+                filesSubmitted,
+                fileSuccess,
+                fileError,
+                fileReset,
+            };
+            stepper
+                .addSteps(self.upload.step);
+
             self.steps.push({
-                titleValue: 'Upload data',
-                stepNumber: 1,
-                isActive: false,
-                isCompleted: false
-            }, {
                 titleValue: 'Confirm data type',
                 stepNumber: 2,
                 isActive: false,
@@ -74,6 +95,10 @@
                 onContinue: () => {
                     console.log(self.dataType);
                     stepper.nextStep();
+
+                    console.log('=>', self.steps[1], this);
+
+                    console.log('__form1', self.__form1);
                 },
                 onCancel: () => {
                     stepper.previousStep();
@@ -88,9 +113,16 @@
                 isActive: false,
                 isCompleted: false,
                 onContinue: () => {
-                    stepper.nextStep();
+                    self.configureLayerForm.$setValidity('required', true, {
+                        a: true
+                    });
+                    self.configureLayerForm.$setValidity('customKey', false, true);
+
+                    // stepper.nextStep();
                 },
                 onCancel: () => {
+                    self.configureLayerForm.$setPristine();
+                    self.configureLayerForm.$setUntouched();
                     stepper.previousStep();
                 }
             });
@@ -100,19 +132,68 @@
                 .start(); // activate stepper on the first step
         }
 
+        function fileSelectorOpened() {
+            /* jshint validthis:true */
+            this.fileReset();
+        }
+
         function filesSubmitted(files, event, flow) {
-            console.log('submitted', files, event, flow);
-            self.file = files[0];
+            /* jshint validthis:true */
+            console.log('submitted', files, event, flow, this);
+            this.file = files[0];
             flow.upload();
         }
 
         function fileSuccess(file, message, flow) {
-            console.log('success', file, message, flow);
+            /* jshint validthis:true */
+            console.log('success', file, message, flow, this);
             $timeout(() => stepper.nextStep(), 300);
         }
 
         function fileError(file, message, flow) {
-            console.log('error', file, message, flow);
+            /* jshint validthis:true */
+            console.log('error', file, flow, this.form);
+            this.form.$setValidity('upload-error', false, true);
+        }
+
+        function fileReset() {
+            /* jshint validthis:true */
+            this.file = null;
+            this.form.$setValidity('upload-error', true, true);
+        }
+    }
+})();
+
+(() => {
+    'use strict';
+
+    /**
+     * @ngdoc directive
+     * @name rvFileValidation
+     * @module app.ui.loader
+     * @restrict A
+     * @description
+     *
+     * The `rvFileValidation` directive description.
+     *
+     */
+    angular
+        .module('app.ui.loader')
+        .directive('rvFileValidation', rvFileValidation);
+
+    function rvFileValidation() {
+        const directive = {
+            restrict: 'A',
+            require: 'ngModel',
+            link: link
+        };
+
+        return directive;
+
+        /**********/
+
+        function link(scope, el, attr, ctrl) {
+            console.log('rvFileValidation', scope, el, attr, ctrl);
         }
     }
 })();
