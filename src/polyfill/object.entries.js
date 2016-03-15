580457e2a6a6e6a7bcc01ada0f012b5e5a100ea9
Aly Merchant <aly.merchant@ec.gc.ca>
2016-03-15 10:28:37 -0400

    feat(core): add Object.entries polyfill

---
+++ src/polyfill/object.entries.js
@@ -0,0 +1,18 @@
+var reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+var isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+var concat = Function.bind.call(Function.call, Array.prototype.concat);
+var keys = Object.keys;
+// apparently using Object.keys is not spec compliant, but it's in a very odd case
+// and using Object.keys makes IE happy https://bugzilla.mozilla.org/show_bug.cgi?id=1208464#c13
+
+if (!Object.values) {
+    Object.values = function values(O) {
+        return reduce(keys(O), (v, k) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), []);
+    };
+}
+
+if (!Object.entries) {
+    Object.entries = function entries(O) {
+        return reduce(keys(O), (e, k) => concat(e, typeof k === 'string' && isEnumerable(O, k) ? [[k, O[k]]] : []), []);
+    };
+}
