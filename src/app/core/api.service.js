c1c4c5cb0de03d50a991b80529e378f5453999df
Spencer Wahl <spencer.wahl@ec.gc.ca>
2016-03-30 11:16:21 -0400

    refactor(core): small PR changes
    Function comments, arrow function styling

--- src/app/core/api.service.js
+++ src/app/core/api.service.js
@@ -26,13 +26,21 @@
 
         /**********************/
 
+        /**
+         * Sets the translation language and reloads the map
+         *
+         * @param {String}  lang    the language to switch to
+         */
         function setLanguage(lang) {
             $translate.use(lang);
             geoService.assembleMap();
         }
 
+        /**
+         * Attaches a promise to the appRegistry which resolves with apiService
+         */
         function init() {
-            const apiPromise = $q((resolve) => {
+            const apiPromise = $q(resolve => {
                 $rootScope.$on(events.rvReady, resolve(service));
                 console.info('registered');
             });
