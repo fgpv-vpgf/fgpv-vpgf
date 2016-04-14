8af30bd56772a287832f1c846f087a6f341e6e83
Spencer Wahl <spencer.wahl@ec.gc.ca>
2016-04-14 10:57:39 -0400

    chore(build): polyfills for ie11

---
+++ src/polyfill/string.startsWith.js
@@ -0,0 +1,12 @@
+// jscs:disable
+/* jshint ignore:start */
+
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function (searchString, position){
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+  };
+}
+
+// jscs:enable
+/* jshint ignore:end */
