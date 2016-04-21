18c07b09632ab5568aa4400d5555614f180a6cf8
Cynthia (Qingwei) Li <Qingwei.Li@ec.gc.ca>
2016-04-21 17:12:17 -0400

    feat(layout): add storage service for div references

---
+++ src/app/layout/storage.service.js
@@ -0,0 +1,32 @@
+(() => {
+    'use strict';
+
+    /**
+     * @ngdoc service
+     * @name storageService
+     * @module app.layout
+     *
+     * @description
+     * The 'storageService' service stores information about the the layout, like width/height of the panels.
+     */
+    angular
+        .module('app.layout')
+        .factory('storageService', storageService);
+
+    function storageService() {
+        const service = {
+            controller: Controller,
+            panels: {},
+            panes: {} // registry for content pane nodes
+        };
+
+        return service;
+
+    }
+
+    function Controller() {
+        const self = this;
+
+        self.storageService = storageService();
+    }
+})();
