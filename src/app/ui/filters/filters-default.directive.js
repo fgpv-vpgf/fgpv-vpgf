9ed85132d6567d27a26d0c281d136ed61cedf9de
Cynthia (Qingwei) Li <Qingwei.Li@ec.gc.ca>
2016-05-13 15:34:01 -0400

    feat(ui): add details icon tooltip and logic

--- src/app/ui/filters/filters-default.directive.js
+++ src/app/ui/filters/filters-default.directive.js
@@ -1,6 +1,7 @@
 (() => {
     // jscs:disable maximumLineLength
     const ZOOM_TO_ICON = tooltip => `<md-icon class="rv-icon rv-zoom-to" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" fit="" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g id="zoom_in"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zm2.5-4h-2v2H9v-2H7V9h2V7h1v2h2v1z"/></g></svg><md-tooltip role="tooltip"><div class="md-content md-show"><span>${tooltip}</span></div></md-tooltip></md-icon>`;
+    const DETAILS_ICON = tooltip => `<md-icon class="rv-icon rv-description" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" fit="" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g id="description"><path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z"></path></g></svg><md-tooltip role="tooltip"><div class="md-content md-show"><span>${tooltip}</span></div></md-tooltip></md-icon>`;
     // jscs:enable maximumLineLength
 
     /**
@@ -51,6 +52,7 @@
              */
             function createTable() {
                 const zoomText = $translate.instant('filter.tooltip.zoom');
+                const descriptionsText = $translate.instant('filter.tooltip.description');
 
                 // TODO: move hardcoded stuff in consts
                 containerNode = containerNode || el.find('.rv-filters-data-container');
@@ -109,7 +111,7 @@
                     // get the first column after the symbol column
                     const interactiveColumn = displayData.columns.find(column =>
                         column.data !== 'rvSymbol');
-                    addColumnInteractivity(interactiveColumn, ZOOM_TO_ICON(zoomText));
+                    addColumnInteractivity(interactiveColumn, [ZOOM_TO_ICON(zoomText), DETAILS_ICON(descriptionsText)]);
                 }
 
                 // ~~I hate DataTables~~ Datatables are cool!
@@ -159,6 +161,33 @@
 
                     geoService.zoomToGraphic(layer, objId);
                 });
+
+                self.table.on('click', 'md-icon.rv-description', event => {
+                    const tr = $(event.target).closest('tr');
+                    const row = self.table.row(tr);
+
+                    // get object id from row data
+                    const objId = row.data()[displayData.oidField];
+                    const detailsObj = {
+                        isLoading: false,
+                        data: [
+                            {
+                                name: geoService.getFeatureName(row.data(), {}, objId),
+                                data: geoService.attributesToDetails(row.data(), displayData.fields)
+                            }
+                        ],
+                        requestId: -1,
+                        requester: {
+                            format: 'EsriFeature',
+                            name: requester.name
+                        }
+                    };
+                    const details = {
+                        data: [detailsObj]
+                    };
+
+                    stateManager.toggleDisplayPanel('mainDetails', details, {}, 0);
+                });
             }
 
             /**
@@ -177,11 +206,11 @@
              * Adds zoom and details buttons to the column provided.
              * @param {Object} column from the formatted attributes bundle
              */
-            function addColumnInteractivity(column, zoomIcon) {
+            function addColumnInteractivity(column, icons) {
                 // use render function to augment button to displayed data when the table is rendered
                 column.render = data => {
                     return `<div class="rv-wrapper rv-icon-16"><span class="rv-data">${data}</span>
-                        ${zoomIcon}</div>`;
+                        ${icons.join('')}</div>`;
                 };
             }
         }
