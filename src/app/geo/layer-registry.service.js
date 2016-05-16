749c655d582ef00f53ba9119ea5a08fab1bba5af
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2016-05-16 07:49:23 -0400

    refactor(core): after-rebase clean up

--- src/app/geo/layer-registry.service.js
+++ src/app/geo/layer-registry.service.js
@@ -100,14 +100,13 @@
             };
             // jscs:enable requireSpacesInAnonymousFunctionExpression
 
-			// FIXME: for debug purposes
+            // FIXME: for debug purposes
             // FIXME: remove
             window.RV._debug = {};
             window.RV._debug.layers = service.layers;
             window.RV._debug.legend = service.legend;
             window.RV._debug.graphicsLayerIds = mapObject.graphicsLayerIds;
             window.RV._debug.layerIds = mapObject.layerIds;
-
 
             // set event handler for extent changes
             gapiService.gapi.events.wrapEvents(
@@ -248,7 +247,7 @@
              * @param {String} targetId the id of the layer the target layer will be moved on top of; can be -1, if its the end of the list
             * @return {Number}          index at which the source layer should be inserted in the map stack
             */
-			function getLayerInsertPosition(sourceId, targetId) {
+            function getLayerInsertPosition(sourceId, targetId) {
                 const sourceEntry = service.layers[sourceId].state;
                 const targetEntry = typeof targetId !== 'undefined' ? service.layers[targetId].state : null;
 
@@ -301,7 +300,7 @@
 
                 _testSyncCheck();
 
-                //console.log(`reodder ${sourceId} on ${targetIndex}`);
+                // console.log(`reodder ${sourceId} on ${targetIndex}`);
                 mapObject.reorderLayer(sourceLayer, targetIndex);
             }
 
@@ -412,7 +411,7 @@
                                 *  index an optional index indicating at which position the layer was added to the map
                                 * (if supplied it is the caller's responsibility to make sure the layer is added in the correct location)
                                 * */
-                                layerRecord._attributeBundle = attributesPromise;
+                                layerRecord.attributeBundle = attributesPromise;
                                 ref.legendService.addLayer(layerRecord, index); // generate actual legend entry
 
                                 // TODO refactor this as it has nothing to do with layer registration;
@@ -420,7 +419,7 @@
                                 const opts = layerRecord.state.options;
                                 if (opts.hasOwnProperty('boundingBox') && opts.boundingBox.value) {
                                     setBboxState(layerRecord.state, true);
-                            }
+                                }
                             }
                         },
                         error: data => {
@@ -525,15 +524,10 @@
 
                 service.layers[layer.id] = layerRecord;
 
-                return layerRecord;
-                }
-
                 // set scale state
                 setScaleDepState(layer.id);
 
-                // FIXME:
-                window.RV.layers = window.RV.layers || {};
-                window.RV.layers[layer.id] = layerRecord;
+                return layerRecord;
             }
 
             /**
