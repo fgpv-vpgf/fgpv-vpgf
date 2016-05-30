9fd5e6494611a9fd66e0597c7c584229d658a259
Petrov <Miles.Petrov@ec.gc.ca>
2016-05-30 14:41:25 -0400

    feat(ui): escape key closes panels in their opening order
    rv-shell directive listens on the window scope instead of $rootScope since default is for focus to return to the HTML <body> tag when a focused element is destroyed. This caused issues with sidePanel that would not allow it to be closed on escape key. This is only a concern with multiple maps on the same page and the user is using keyboard navigation. The point information "mainDetails" panel cannot be closed without also closing the side panel due to how child panels are processed in stateManager (currently a childs parent is closed by default).
    
    Closes #622

--- src/app/layout/shell.directive.js
+++ src/app/layout/shell.directive.js
@@ -16,7 +16,7 @@
         .module('app.layout')
         .directive('rvShell', rvShell);
 
-    function rvShell(storageService, stateManager, $rootElement) {
+    function rvShell(storageService, stateManager) {
         const directive = {
             restrict: 'E',
             templateUrl: 'app/layout/shell.html',
@@ -31,13 +31,10 @@
 
         function link(scope, el) {
             storageService.panels.shell = el;
-
-            // close all panels when escape key is pressed
-            $rootElement.bind('keydown', event => {
+            $(window).bind('keydown', event => {
                 if (event.which === 27) {
                     scope.$apply(() => {
-                        Object.keys(stateManager.state)
-                            .forEach(pName => stateManager.setActive({ [pName]: false }));
+                        stateManager.closePanelFromHistory();
                     });
                 }
             });
