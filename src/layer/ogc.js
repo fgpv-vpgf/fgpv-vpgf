996c1b92e9df634f378286bbcbecc68b7c9f530e
Aly Merchant <aly.merchant@ec.gc.ca>
2016-06-01 15:42:09 -0400

    feat(layer): port RAMP GetCapabilities parser
    relates fgpv-vpgf/fgpv-vpgf#610

--- src/layer/ogc.js
+++ src/layer/ogc.js
@@ -57,6 +57,69 @@ function getFeatureInfoBuilder(esriBundle) {
     };
 }
 
+function parseCapabilitiesBuilder(esriBundle) {
+
+    /**
+     * Fetch layer data from a WMS endpoint.  This method will execute a WMS GetCapabilities
+     * request against the specified URL, it requests WMS 1.3 and it is capable of parsing
+     * 1.3 or 1.1.1 responses.  It returns a promise which will resolve with basic layer
+     * metadata and querying information.
+     *
+     * metadata response format:
+     *   { queryTypes: [mimeType], layers: [{name, desc, queryable(bool)}] }
+     *
+     * @param {string} wmsEndpoint a URL pointing to a WMS server (it must not include a query string)
+     * @return {Promise} a promise resolving with a metadata object (as specified above)
+     */
+    return (wmsEndpoint) => {
+        const reqPromise = Promise.resolve(new esriBundle.EsriRequest({
+            url: wmsEndpoint + '?service=WMS&version=1.3&request=GetCapabilities',
+            handleAs: 'xml'
+        }).promise);
+
+        // there might already be a way to do this in the parsing API
+        // I don't know XML parsing well enough (and I don't want to)
+        // this has now been ported from RAMP to FGPV and I still, happily,
+        // do not know any more about XML parsing now
+        function getImmediateChild(node, childName) {
+            for (let i = 0; i < node.childNodes.length; ++i) {
+                if (node.childNodes[i].nodeName === childName) {
+                    return node.childNodes[i];
+                }
+            }
+            return undefined;
+        }
+
+        reqPromise.then(data => {
+            var layers, res = {};
+
+            const namedlayers = query('Layer > Name', data).map((nameNode) { return nameNode.parentNode; });
+            res.layers = layers.map(function (x) {
+                const nameNode = getImmediateChild(x, 'Name');
+                const name = nameNode.textContent || nameNode.text; // .text is for IE9's benefit, even though it claims to support .textContent
+                const titleNode = getImmediateChild(x, 'Title');
+                return {
+                    name: name,
+                    desc: titleNode ? (titleNode.textContent || titleNode.text) : name,
+                    queryable: x.getAttribute('queryable') === '1'
+                };
+            });
+            res.queryTypes = query('GetFeatureInfo > Format', data).map(function (node) { return node.textContent || node.text; });
+
+            return res;
+        })
+
+            function (error) {
+                console.log(error);
+                def.reject(error);
+            }
+        );
+
+        return def.promise;
+    }
+
+}
+
 /**
  * Finds the appropriate legend URLs for WMS layers.
  *
