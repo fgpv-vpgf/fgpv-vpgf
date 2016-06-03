0ae77485f0cb08d6961568334bce2416766fca33
Aly Merchant <aly.merchant@ec.gc.ca>
2016-06-03 15:40:31 -0400

    feat(layer): update parser to output a tree
    GetCapabilities parser now outputs a tree structure.
    
    relates fgpv-vpgf/fgpv-vpgf#610

--- src/layer/ogc.js
+++ src/layer/ogc.js
@@ -59,6 +59,8 @@ function getFeatureInfoBuilder(esriBundle) {
 
 function parseCapabilitiesBuilder(esriBundle) {
 
+    const query = esriBundle.dojoQuery;
+
     /**
      * Fetch layer data from a WMS endpoint.  This method will execute a WMS GetCapabilities
      * request against the specified URL, it requests WMS 1.3 and it is capable of parsing
@@ -66,13 +68,16 @@ function parseCapabilitiesBuilder(esriBundle) {
      * metadata and querying information.
      *
      * metadata response format:
-     *   { queryTypes: [mimeType], layers: [{name, desc, queryable(bool)}] }
+     *   { queryTypes: [mimeType(str)],
+     *     layers: [
+     *       {name(str), desc(str), queryable(bool), layers:[recursive] }
+     *     ] }
      *
      * @param {string} wmsEndpoint a URL pointing to a WMS server (it must not include a query string)
      * @return {Promise} a promise resolving with a metadata object (as specified above)
      */
     return (wmsEndpoint) => {
-        const reqPromise = Promise.resolve(new esriBundle.EsriRequest({
+        const reqPromise = Promise.resolve(new esriBundle.esriRequest({
             url: wmsEndpoint + '?service=WMS&version=1.3&request=GetCapabilities',
             handleAs: 'xml'
         }).promise);
@@ -90,33 +95,24 @@ function parseCapabilitiesBuilder(esriBundle) {
             return undefined;
         }
 
-        reqPromise.then(data => {
-            var layers, res = {};
-
-            const namedlayers = query('Layer > Name', data).map((nameNode) { return nameNode.parentNode; });
-            res.layers = layers.map(function (x) {
-                const nameNode = getImmediateChild(x, 'Name');
-                const name = nameNode.textContent || nameNode.text; // .text is for IE9's benefit, even though it claims to support .textContent
-                const titleNode = getImmediateChild(x, 'Title');
+        function getLayers(xmlNode) {
+            return query('> Layer', xmlNode).map(layer => {
+                const nameNode = getImmediateChild(layer, 'Name');
+                const titleNode = getImmediateChild(layer, 'Title');
                 return {
-                    name: name,
-                    desc: titleNode ? (titleNode.textContent || titleNode.text) : name,
-                    queryable: x.getAttribute('queryable') === '1'
+                    name: nameNode ? nameNode.textContent : null,
+                    desc: titleNode.textContent,
+                    queryable: layer.getAttribute('queryable') === '1',
+                    layers: getLayers(layer)
                 };
             });
-            res.queryTypes = query('GetFeatureInfo > Format', data).map(function (node) { return node.textContent || node.text; });
+        }
 
-            return res;
-        })
-
-            function (error) {
-                console.log(error);
-                def.reject(error);
-            }
-        );
-
-        return def.promise;
-    }
+        return reqPromise.then(data => ({
+            layers: getLayers(query('Capability', data)[0]),
+            queryTypes: query('GetFeatureInfo > Format', data).map(node => node.textContent)
+        }));
+    };
 
 }
 
@@ -137,6 +133,7 @@ module.exports = esriBundle => {
     return {
         WmsLayer: esriBundle.WmsLayer,
         getFeatureInfo: getFeatureInfoBuilder(esriBundle),
+        parseCapabilities: parseCapabilitiesBuilder(esriBundle),
         getLegendUrls
     };
 };
