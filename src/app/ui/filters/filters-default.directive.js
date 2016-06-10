cacbe02e4073bec6979a6de8bd763745263d5dfe
Cynthia (Qingwei) Li <Qingwei.Li@ec.gc.ca>
2016-06-10 10:14:45 -0400

    feat(ui): change zoom to disabled when custom projection detected

--- src/app/ui/filters/filters-default.directive.js
+++ src/app/ui/filters/filters-default.directive.js
@@ -1,6 +1,6 @@
 (() => {
     // jscs:disable maximumLineLength
-    const ZOOM_TO_ICON = tooltip => `<md-icon class="rv-icon rv-zoom-to" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" fit="" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g id="zoom_in"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zm2.5-4h-2v2H9v-2H7V9h2V7h1v2h2v1z"/></g></svg><md-tooltip role="tooltip"><div class="md-content md-show"><span>${tooltip}</span></div></md-tooltip></md-icon>`;
+    let ZOOM_TO_ICON;
     const DETAILS_ICON = tooltip => `<md-icon class="rv-icon rv-description" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" fit="" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g id="description"><path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z"></path></g></svg><md-tooltip role="tooltip"><div class="md-content md-show"><span>${tooltip}</span></div></md-tooltip></md-icon>`;
     // jscs:enable maximumLineLength
 
@@ -53,7 +53,7 @@
              * Creates a new datatables instance (destroying existing if any). It pulls the data from the stateManager display store.
              */
             function createTable() {
-                const zoomText = $translate.instant('filter.tooltip.zoom');
+                let zoomText = $translate.instant('filter.tooltip.zoom');
                 const descriptionsText = $translate.instant('filter.tooltip.description');
 
                 // TODO: move hardcoded stuff in consts
@@ -62,6 +62,15 @@
 
                 const requester = stateManager.display.filters.requester;
                 const displayData = stateManager.display.filters.data;
+
+                // jscs:disable maximumLineLength
+                if (!geoService.validateProj(geoService.layers[requester.layerId]._layer.spatialReference)) {
+                    ZOOM_TO_ICON = tooltip => `<md-icon class="rv-zoom-to disabled" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" fit="" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g id="zoom_in"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zm2.5-4h-2v2H9v-2H7V9h2V7h1v2h2v1z"/></g></svg><md-tooltip role="tooltip"><div class="md-content md-show"><span>${tooltip}</span></div></md-tooltip></md-icon>`;
+                    zoomText = $translate.instant('filter.tooltip.nozoom');
+                } else {
+                    ZOOM_TO_ICON = tooltip => `<md-icon class="rv-icon rv-zoom-to" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" fit="" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g id="zoom_in"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zm2.5-4h-2v2H9v-2H7V9h2V7h1v2h2v1z"/></g></svg><md-tooltip role="tooltip"><div class="md-content md-show"><span>${tooltip}</span></div></md-tooltip></md-icon>`;
+                }
+                // jscs:enable maximumLineLength
 
                 // forced delay of a 100 to prevent the loading indicator from flickering if the table is created too fast; it's annoying; it means that switching tables takes at least 100ms no matter how small the table is; in majority of cases it should take more than 100ms to get data and create a table anyway;
                 const forcedDelay = $q(fulfill =>
