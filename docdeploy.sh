1deca01724fbb6af36b67eaad45c01dd163c30cc
Aly Merchant <aly.merchant@ec.gc.ca>
2016-06-13 15:12:46 -0400

    chore(build): add docdeploy to Travis build

--- docdeploy.sh
+++ docdeploy.sh
@@ -2,17 +2,19 @@
 
 set -e
 
-if [ "$TRAVIS_REPO_SLUG" == "fgpv-vpgf/fgpv-vpgf" && -n "$TRAVIS_TAG" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "fgpv-vpgf/fgpv-vpgf" ] && [ -n "$TRAVIS_TAG" ]; then
     gulp dgeni
+    # this section assumes the id_rsa key has already been decrypted
+    # devdeploy.sh should run before this script
     echo -e "Host *\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
     eval `ssh-agent -s`
-    ssh-add deploykey_rsa
+    ssh-add ~/.ssh/id_rsa
     git clone --depth=1 git@github.com:fgpv-vpgf/fgpv-vpgf.github.io.git ghdocs
     mkdir -p ghdocs/fgpv/$TRAVIS_TAG
     rsync -av --delete dist/docs/app/ ghdocs/fgpv/$TRAVIS_TAG/
     cd ghdocs
     git add fgpv/$TRAVIS_TAG
-    git config user.email "glitch-bot@example.com"
+    git config user.email "glitch.chatbot@gmail.com"
     git config user.name "Glitch Bot"
     git commit -m "Docs for fgpv@$TRAVIS_TAG"
     git push
