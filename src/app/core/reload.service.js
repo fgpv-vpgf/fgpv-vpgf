adf6d12beeeb3759bec07a386162cd98a835a123
Spencer Wahl <spencer.wahl@ec.gc.ca>
2016-06-22 10:47:08 -0400

    docs(core): reloadService docs

--- src/app/core/reload.service.js
+++ src/app/core/reload.service.js
@@ -1,29 +1,52 @@
-angular
-    .module('app.core')
-    .factory('reloadService', reloadService);
+(() => {
 
-function reloadService($translate, bookmarkService, geoService, configService) {
-    const service = {
-        loadNewProjection,
-        loadNewLang
-    };
+    /**
+     * @ngdoc service
+     * @name reloadService
+     * @module app.core
+     *
+     * @description reloadService manages state through map reloads
+     *
+     */
+    angular
+        .module('app.core')
+        .factory('reloadService', reloadService);
 
-    return service;
+    function reloadService($translate, bookmarkService, geoService, configService) {
+        const service = {
+            loadNewProjection,
+            loadNewLang
+        };
 
-    /************************/
+        return service;
 
-    function loadNewProjection(basemapId) {
-        bookmarkService.updateConfig();
-        geoService.setSelectedBaseMap(basemapId);
-        geoService.assembleMap();
+        /************************/
+
+        /**
+         * Maintains state over projection switch. Updates the config to the current state,
+         * sets the new basemap, and then reloads the map.
+         *
+         * @param {String} basemapId     The id of the basemap to switch to
+         */
+        function loadNewProjection(basemapId) {
+            bookmarkService.updateConfig();
+            geoService.setSelectedBaseMap(basemapId);
+            geoService.assembleMap();
+        }
+
+        /**
+         * Maintains state over language switch. Gets the current state, switches to the new lang and its config,
+         * and then reloads the map.
+         *
+         * @param {String} lang      The code of the language to switch to
+         */
+        function loadNewLang(lang) {
+            const bookmark = bookmarkService.getBookmark();
+            $translate.use(lang);
+            configService.getCurrent(config => {
+                bookmarkService.parseBookmark(bookmark, config);
+            });
+            geoService.assembleMap();
+        }
     }
-
-    function loadNewLang(lang) {
-        const bookmark = bookmarkService.getBookmark();
-        $translate.use(lang);
-        configService.getCurrent(config => {
-            bookmarkService.parseBookmark(bookmark, config);
-        });
-        geoService.assembleMap();
-    }
-}
+})();
