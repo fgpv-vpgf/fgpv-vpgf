57b339fa92bcc6da2cfe46891618ec7b9c98e4e6
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2016-06-27 13:33:22 -0400

    feat(ui): improve wms legend animation

--- src/app/ui/toc/entry.directive.js
+++ src/app/ui/toc/entry.directive.js
@@ -59,8 +59,6 @@
                 // call toggleGroup function on the tocController with the group object (see template)
                 self.defaultAction = tocService.actions.toggleLayerGroup;
             }
-
-            self.itemNameOnTop = self.entry.layerType === 'ogcWms';
         }
     }
 })();
