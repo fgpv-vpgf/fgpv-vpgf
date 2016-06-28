bca02e141422fc31495ca5d5ff1d952ae2f7791c
Aly Merchant <aly.merchant@ec.gc.ca>
2016-06-28 09:31:33 -0400

    chore(build): add missing icons
    relates #768

--- gulp.config.js
+++ gulp.config.js
@@ -166,7 +166,7 @@ module.exports = function () {
         { file: 'content/images/iconsets/content-icons.svg', prefix: 'content', icons: 'create add remove'.split(' ') },
         { file: 'content/images/iconsets/editor-icons.svg', prefix: 'editor', icons: 'insert_drive_file'.split(' ') },
         { file: 'content/images/iconsets/file-icons.svg', prefix: 'file', icons: 'file_upload cloud'.split(' ') },
-        { file: 'content/images/iconsets/hardware-icons.svg', prefix: 'hardware', icons: 'keyboard_arrow_right'.split(' ') },
+        { file: 'content/images/iconsets/hardware-icons.svg', prefix: 'hardware', icons: 'keyboard_arrow_right keyboard_arrow_down'.split(' ') },
         { file: 'content/images/iconsets/image-icons.svg', prefix: 'image', icons: 'tune photo'.split(' ') },
         { file: 'content/images/iconsets/maps-icons.svg', prefix: 'maps', icons: 'place layers my_location map'.split(' ') },
         { file: 'content/images/iconsets/navigation-icons.svg', prefix: 'navigation', icons: 'menu check more_vert close more_horiz refresh'.split(' ') },
