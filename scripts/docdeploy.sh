cd962334ab40acae467b7d1d0171cbe86925f25f
Aly Merchant <aly.merchant@ec.gc.ca>
2016-07-19 15:49:36 -0400

    chore(build): add docdeploy script
    relates fgpv-vpgf/fgpv-vpgf#910

---
+++ scripts/docdeploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+if [ "$TRAVIS_REPO_SLUG" == "fgpv-vpgf/geoApi" ] && [ -n "$TRAVIS_TAG" ]; then
+    npm run doc
+    # this section assumes the id_rsa key has already been decrypted
+    # devdeploy.sh should run before this script
+    echo -e "Host *\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
+    eval `ssh-agent -s`
+    ssh-add ~/.ssh/id_rsa
+    git clone --depth=1 git@github.com:fgpv-vpgf/rcs.git -b gh-pages ghdocs
+    mkdir -p ghdocs/$TRAVIS_TAG
+    rsync -av --delete docbuild/ ghdocs/$TRAVIS_TAG/
+    bash ./scripts/make_doc_index.sh ghdocs/ > ghdocs/index.html
+    cd ghdocs
+    git add $TRAVIS_TAG
+    git add index.html
+    git config user.email "glitch.chatbot@gmail.com"
+    git config user.name "Glitch Bot"
+    git commit -m "Docs for geoApi@$TRAVIS_TAG"
+    git push
+    cd ..
+    rm -rf ghdocs
+fi
