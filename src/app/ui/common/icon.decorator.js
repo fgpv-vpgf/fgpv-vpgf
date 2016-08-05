68f47d6b11559fde335f26418f1a1d1742d0d030
Miles Petrov <milesap@gmail.com>
2016-08-05 10:22:33 -0400

    fix(ui): svg elements inside buttons no longer focusable in IE 11
    Closes #1031

---
+++ src/app/ui/common/icon.decorator.js
@@ -0,0 +1,54 @@
+(() => {
+    'use strict';
+
+    /**
+     * @module mdIconDirectiveDecorator
+     * @memberof material.components.icon
+     * @requires $delegate
+     * @requires $timeout
+     * @description
+     *
+     * The `mdIconDirectiveDecorator` decorates vanilla `mdIconDirective`.
+     * It sets the svg childNode property 'focusable' to false so that when tabbing in IE 11 the svg element is not focusable.
+     * Previously, both the button and the svg icon would gain focus while tabbing in IE 11 making the user tab twice to get to the
+     * next button.
+     */
+
+    angular
+        .module('material.components.icon')
+        .decorator('mdIconDirective', mdIconDirectiveDecorator);
+
+    function mdIconDirectiveDecorator($delegate, $timeout) {
+        'ngInject';
+
+        const mdIconDirective = $delegate[0];
+        const originalCompile = mdIconDirective.compile; // store reference to its compile function
+        mdIconDirective.compile = decorateCompile(originalCompile); // decorate compile function
+
+        return $delegate;
+
+        /**
+         * Decorates the original mdIcon compile functions.
+         * @function decorateCompile
+         * @param  {Function} originalCompile original compile function
+         * @return {Function}                 enhances link function returned by the decorated compile function which fixes IE 11 tab issues
+         */
+        function decorateCompile(originalCompile) {
+            return (...args) => {
+                const originalLink = originalCompile(...args);
+
+                // return a decorated link function
+                return (scope, el, attrs, ctrls) => {
+                    // call the original link function
+                    originalLink(scope, el, attrs, ctrls);
+
+                    // $timeout needed since there is a delay md-icon has when adding svg elements
+                    // see https://github.com/angular/material/blob/master/src/components/icon/js/iconDirective.js#L236
+                    $timeout(() => {
+                        el[0].childNodes[0].setAttribute('focusable', false);
+                    });
+                };
+            };
+        }
+    }
+})();
