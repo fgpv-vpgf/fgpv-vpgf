cddf666abcd728e7b12990ded01ee62c6d1b442b
Miles Petrov <milesap@gmail.com>
2016-09-22 10:30:09 -0400

    fix(ui): disable mouse focus support

--- src/app/layout/focus.service.js
+++ src/app/layout/focus.service.js
@@ -49,13 +49,9 @@
         $(document).on('mousedown', event => {
             dlg.hide(); // always hide the dialog
 
-            // clicking inside the map disables focus manager until focus leaves the viewer
+            // disable if clicking on the viewer until mouse support is implemented
             if ($.contains($rootElement[0], event.target)) {
-                setStatus('ACTIVE');
-            } else if (!isFullPage) {
-                setStatus('NOT_ACTIVE');
-            } else {
-                setStatus(); // disable completely when in full-screen
+                setStatus();
             }
         });
 
