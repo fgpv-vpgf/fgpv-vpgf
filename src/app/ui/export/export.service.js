a9d430d1541a8cff29f2c46b9420ba80d09c406e
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2016-10-17 12:27:00 -0400

    feat(export): add file save library to save export image

--- src/app/ui/export/export.service.js
+++ src/app/ui/export/export.service.js
@@ -1,3 +1,4 @@
+/* global saveAs */
 (() => {
     'use strict';
 
@@ -53,7 +54,7 @@
             $mdDialog.hide();
         }
 
-        function ExportController($rootElement, $q, storageService, exportLegendService, geoService, gapiService) {
+        function ExportController($rootElement, $q, configService, storageService, exportLegendService, geoService, gapiService) {
             'ngInject';
             const self = this;
 
@@ -68,24 +69,29 @@
             self.isGenerationComplete = false;
 
             self.isLegendIncluded = false;
-            self.includeLegend = includeLegend;
             self.lengendGraphic = null;
 
+            // functions
+            self.saveImage = saveImage;
+            self.includeLegend = includeLegend;
             self.close = service.close;
 
-            const { serverPromise, localPromise } = gapiService.gapi.mapPrint.print(geoService.mapObject, {
-                url: 'http://geoappext.nrcan.gc.ca/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task',
-                format: 'png32'
-            });
+            // start the print task
+            configService.getCurrent().then(config => {
+                const { serverPromise, localPromise } = gapiService.gapi.mapPrint.print(geoService.mapObject, {
+                    url: config.services.exportMapUrl,
+                    format: 'png32'
+                });
 
-            $q.resolve(serverPromise).then(canvas =>
-                self.serviceGraphic = canvas);
+                $q.resolve(serverPromise).then(canvas =>
+                    self.serviceGraphic = canvas);
 
-            $q.resolve(localPromise).then(canvas =>
-                self.localGraphic = canvas);
+                $q.resolve(localPromise).then(canvas =>
+                    self.localGraphic = canvas);
 
-            $q.all([serverPromise, localPromise]).then(result => {
-                self.isGenerationComplete = true;
+                $q.all([serverPromise, localPromise]).then(result => {
+                    self.isGenerationComplete = true;
+                });
             });
 
             /***/
@@ -97,7 +103,43 @@
              */
             function includeLegend() {
                 if (!self.legendGraphic && self.isLegendIncluded) {
-                    self.legendGraphic = exportLegendService.generate(mapWidth, 350).node;
+                    exportLegendService.generate(mapWidth, 350)
+                        .then(graphic =>
+                            self.legendGraphic = graphic);
+                    // self.legendGraphic = exportLegendService.generate(mapWidth, 350).node;
+                }
+            }
+
+            function saveImage() {
+                const gutter = 20;
+
+                const canvas = document.createElement('canvas')
+                canvas.width = self.dummyGraphic.width + gutter * 2;
+                canvas.height = self.dummyGraphic.height + gutter * 2;
+
+                const context = canvas.getContext('2d');
+                fillCanvas();
+
+                if (self.isLegendIncluded) {
+                    canvas.height = canvas.height + self.legendGraphic.height;
+
+                    fillCanvas();
+                    context.drawImage(self.legendGraphic, gutter, self.dummyGraphic.height + gutter);
+                }
+
+                context.drawImage(self.serviceGraphic, gutter, gutter);
+                context.drawImage(self.localGraphic, gutter, gutter);
+
+                // context.drawImage(self.legendGraphic, 0, mapHeight);
+                // draw to canvas...
+                // canvas = context.canvas;
+                canvas.toBlob(blob => {
+                    saveAs(blob, Math.random() + "pretty image.png");
+                });
+
+                function fillCanvas() {
+                    context.fillStyle = '#fff';
+                    context.fillRect(0, 0, canvas.width, canvas.height);
                 }
             }
         }
