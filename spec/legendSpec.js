632ed16b087ac2e1349e5221ca29dd47be37f22b
Aly Merchant <aly.merchant@ec.gc.ca>
2016-10-19 21:23:58 -0400

    test(legend): add a couple of corner cases
    relates fgpv-vpgf/fgpv-vpgf#1271

--- spec/legendSpec.js
+++ spec/legendSpec.js
@@ -40,21 +40,34 @@ describe('Legend', () => {
         it('should work for more layers than sections (4,2,2), 2', () => {
             const res = legend.makeLegend([4,2,2].map(n => ({height: n})), 2);
             const expected = [false, true, false];
-            expected.forEach((x,i) => expect(res[i].splitBefore).toBe(x));
+            expected.forEach((x,i) => expect(res.layers[i].splitBefore).toBe(x));
         });
 
         it('should work for more layers than sections (4,2,2,4), 3', () => {
             const res = legend.makeLegend([4,2,2,4].map(n => ({height: n})), 3);
             const expected = [false, true, false, true];
-            expected.forEach((x,i) => expect(res[i].splitBefore).toBe(x));
+            expected.forEach((x,i) => expect(res.layers[i].splitBefore).toBe(x));
         });
 
         it('should work for more sections than layers', () => {
             const testdata = JSON.parse(JSON.stringify(data));
-            legend.makeLegend(testdata, 4);
+            legend.makeLegend(testdata, 4, 200);
             expect(testdata[1].splitBefore).toBe(true);
             expect(testdata[2].splitBefore).toBe(true);
             expect(testdata[1].items[1].splitBefore).toBe(true);
+        });
+
+        it('should do nothing fancy for very tall maps', () => {
+            const testdata = JSON.parse(JSON.stringify(data));
+            const res = legend.makeLegend(testdata, 4, 2000);
+            expect(res.sectionsUsed).toBe(3);
+        });
+
+        it('should use simple allocation when given lots of layers', () => {
+            const res = legend.makeLegend(Array(20).fill(4).map(n => ({height: n})), 3, 20);
+            expect(res.sectionsUsed).toBe(3);
+            expect(res.layers[7].splitBefore).toBe(true);
+            expect(res.layers[14].splitBefore).toBe(true);
         });
         
     });
