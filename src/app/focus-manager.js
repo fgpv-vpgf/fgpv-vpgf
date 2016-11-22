d916580e71f8b56b9f049a44ed4281ac50ca273e
Miles Petrov <milesap@gmail.com>
2016-11-22 08:25:23 -0500

    fix(ui): sidenav styles are no longer broken
    Closes #1383

--- src/app/focus-manager.js
+++ src/app/focus-manager.js
@@ -10,8 +10,9 @@
         ACTIVE: 'ACTIVE'
     };
     // jQuery selectors for elements that are likely focusable
-    const focusSelector = `a[href], area[href], input:not([disabled]), select:not([disabled]),
-            textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex], [contenteditable]`;
+    // since we don't want focus on md-sidenav (focus instead on close button) we omit this with [tabindex]:not(md-sidenav)
+    const focusSelector = `a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]),
+        button:not([disabled]), iframe, object, embed, [tabindex]:not(md-sidenav), [contenteditable]`;
     // object containing all currently depressed keyboard keys
     const keys = {};
     // ordered list of elements which has received focus
