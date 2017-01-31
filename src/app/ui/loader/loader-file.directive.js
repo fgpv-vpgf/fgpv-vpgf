518308980a8dc0f03ad90ff574b4416c09ebab2c
Johann Levesque <johann.levesque@canada.ca>
2017-01-31 16:58:21 -0500

    fix(loader): keyboard user issues in add layer
    Closes #1595

--- src/app/ui/loader/loader-file.directive.js
+++ src/app/ui/loader/loader-file.directive.js
@@ -27,7 +27,8 @@
         return directive;
     }
 
-    function Controller($scope, $q, $timeout, stateManager, Stepper, geoService, LayerBlueprint, Geo, $rootElement) {
+    function Controller($scope, $q, $timeout, stateManager, Stepper, geoService, LayerBlueprint, Geo, $rootElement,
+        keyNames) {
         'ngInject';
         const self = this;
 
@@ -49,7 +50,9 @@
                 isCompleted: false,
                 onContinue: uploadOnContinue,
                 onCancel: () => onCancel(self.upload.step),
-                reset: uploadReset
+                onKeypress: (event) => { if (event.keyCode === keyNames.ENTER) { uploadOnContinue(); } }, // check if enter key have been pressed and call the next step if so
+                reset: uploadReset,
+                focus: 'dataUpload'
             },
             form: null,
             file: null,
@@ -74,7 +77,8 @@
                 isCompleted: false,
                 onContinue: selectOnContinue,
                 onCancel: () => onCancel(self.select.step),
-                reset: selectReset
+                reset: selectReset,
+                focus: 'dataType'
             },
             selectResetValidation,
             form: null
@@ -88,7 +92,8 @@
                 isCompleted: false,
                 onContinue: configureOnContinue,
                 onCancel: () => onCancel(self.configure.step),
-                reset: configureReset
+                reset: configureReset,
+                focus: 'layerName'
             },
             configureResetValidation,
             colourPickerSettings: {
