529c51e1ce92dbfcfd63153c39629131dc554005
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2017-01-31 16:05:58 -0500

    feat(ui): enhance geosearch ui
    Closes #1495

--- src/app/ui/geosearch/geosearch.service.js
+++ src/app/ui/geosearch/geosearch.service.js
@@ -6,302 +6,144 @@
      * @memberof app.ui
      * @description
      *
-     * The `geosearchService` is responsible for applying geosearch
+     * The `geosearchService` is responsible for opening and closing geosearch panel and to running queries.
      *
      */
     angular
-        .module('app.ui.geosearch')
+        .module('app.ui')
         .factory('geosearchService', geosearchService);
 
-    function geosearchService(stateManager, storageService, debounceService, geoSearch, $rootScope, geoService) {
+    function geosearchService($q, $rootScope, stateManager, storageService, geoSearch, events, debounceService) {
 
-        // main panel to know where to switch when we close geosearch
-        const mainPanel = storageService.panels.shell.find('[rv-state=main]');
+        const service = {
+            isLoading: false, // waiting for results
+            isResultsVisible: false, // showing results when we get some
+            noResultsSearchValue: '', // the (previous) search term which returned no results
 
-        // the extent change event watch
-        let extentEvent;
+            searchValue: '', // current search term
+            searchResults: [], // currect search results
 
-        // debounce the toggle geosearch button to avoid wierd behaviour
-        const debToggleGeo = debounceService.registerDebounce(() => {
-            stateManager.setActive({ side: false }, 'mainGeosearch');
-            mainPanel.addClass('rv-geosearch-filters');
+            runQuery,
 
-            // validate extent to see if visible filter should be disableSearch
-            validateExtent();
+            toggle: toggleBuilder(),
+            onClose,
+            zoomTo
+        };
 
-            if (extentEvent) {
-                // deregister the event listener
-                extentEvent();
-                extentEvent = undefined;
-            } else {
-                // subscribe to 'extentChange' event only if the handle is undefined
-                extentEvent = $rootScope.$on('extentChange',
-                    debounceService.registerDebounce(onExtentChange, 300, false));
+        const ref = {
+            mainPanel: storageService.panels.shell.find('[rv-state=main]'),
+
+            runningRequestCount: 0 //
+        };
+
+        // this is horrible, but `onCloseCallback` stateManager function doesn't work correctly
+        $rootScope.$watch(() => stateManager.state.mainGeosearch.active, newValue => {
+            if (!newValue) {
+                // this will properly hide geosearch content
+                onClose();
             }
         });
 
-        // filters values from geoSearch to be use in geosearch-filters
-        const filters = {
-            provinces: [],
-            types: [],
-            extent: {
-                value: false,
-                outside: false,
-                selection: false
-            }
-        };
-
-        // text value to search for
-        const searchText = {
-             value: ''
-         };
-
-        // hold results from the search
-        const results = {
-             values: { }
-         };
-
-        const service = {
-            filters,
-            searchText,
-            results,
-            zoomSearchExtent,
-            queryGeosearch,
-            clearSearch,
-            getProvinces,
-            getTypes,
-            setProvince,
-            setType,
-            setExtent,
-            toggleToc,
-            toggleGeosearch
-        };
-
-        stateManager.setCloseCallback('mainGeosearch', closeGeosearch);
-
-        init();
-
         return service;
 
-        /************************/
-
         /**
-         * Set the geosearch extent filters values and set extent-change event.
+         * Builds a debounced toggle function to open and close geosearch content
          *
-         * @function init
+         * @function toggleBuilder
          * @private
+         * @return {Function} a geosearch toggle function
          */
-        function init() {
-            // set show visible filter (extent)
-            geoSearch.setExtent('canada');
+        function toggleBuilder() {
+            return debounceService.registerDebounce(() => {
+                ref.mainPanel.addClass('geosearch');
+
+                // skip animation on the main panel when switching to and from geosearch content
+                stateManager.state.main.activeSkip = true;
+                stateManager.state.main.activeSkipOverride = true;
+
+                stateManager.setActive({ side: false }, 'mainGeosearch');
+            });
         }
 
         /**
-         * Zoom to geosearch selection.
+         * Properly closes the geosearch content and restores previous panel if any.
          *
-         * @function zoomSearchExtent
-         * @private
-         * @param   {Object}    item  the item to zoom to.
+         * @function onClose
          */
-        function zoomSearchExtent(item) {
-            geoSearch.zoomSearchExtent(item.bbox, item.position);
-        }
+        function onClose() {
+            // clear the search term when closing
+            service.searchValue = '';
 
-        /**
-         * Query results for geaosearch text from geo-search.service
-         *
-         * @function queryGeosearch
-         */
-        function queryGeosearch() {
-            // query only if there is a least 3 characters, otherwise clear results array to clear content.
-            if (searchText.value.length > 2) {
-                // if there is results, show them. If not, clear results array to clear content.
-                // add * wildcard to the query to make sure we always have results. If not something like
-                // vancouv will return no result.
-                geoSearch.query(`${searchText.value}*`).then((items) => setResults(items))
-                    .then((response) => results.values = response);
-            } else {
-                results.values = [];
-            }
-        }
-
-        /**
-         * Set results object with reponse from the query to show them in geosearch-content.
-         *
-         * @function setResults
-         * @private
-         * @param   {Array} items   the items array to show in geosearch-content panel.
-         * @return  {Array} results the results from the query (empty array if there is no results)
-         */
-        function setResults(items) {
-            if (items.results === undefined) {
-                return [];
-            }
-
-            // extract usefull informations for every item in the array
-            return items.results.map((item) => ({
-                name: item.name,
-                location: `, ${item.location.province.name.split(',')[0]} ${(item.location.city !== null) ?
-                    `, ${item.location.city}` : ''}`,
-                type: item.type.name,
-                bbox: item.bbox,
-                position: item.position
-            }));
-        }
-
-        /**
-         * Called on map extent changes to refresh query if extent is not canada
-         *
-         * @function onExtentChange
-         * @private
-         */
-        function onExtentChange() {
-            validateExtent();
-
-            if (filters.extent.value && searchText.value !== '') {
-                queryGeosearch();
-            }
-        }
-
-        function validateExtent() {
-            // check if visibile extent is inside full extent. If not, disable
-            // extent filters and use full extent.
-            if (!geoService.validateExtent(1.15)) {
-                filters.extent.value = false;
-                filters.extent.outside = true;
-                geoSearch.setExtent('canada');
-            } else {
-                filters.extent.value = filters.extent.selection;
-                filters.extent.outside = false;
-                geoSearch.setExtent((filters.extent.value) ? 'visible' : 'canada');
-            }
-        }
-
-        /**
-         * Clear search text and results values when user click on the close icon from the geosearch text entry
-         *
-         * @function clearSearch
-         */
-        function clearSearch() {
-            searchText.value = '';
-            results.values = [];
-        }
-
-        /**
-         * Set provinces filter values from geo-search.service
-         *
-         * @function getProvinces
-         */
-        function getProvinces() {
-            // only call if filters is empty
-            if (!filters.provinces.length) {
-                // get and set province filter
-                geoSearch.getProvinces().then((values) => {
-                    values.forEach((obj) => {
-                        filters.provinces.push(obj.name);
-                    });
-                });
-            }
-        }
-
-        /**
-         * Set types filter values from geo-search.service
-         *
-         * @function getTypes
-         */
-        function getTypes() {
-            // only call if filters is empty
-            if (!filters.types.length) {
-                // get and set type filter
-                geoSearch.getTypes().then((values) => {
-                    values.forEach((obj) => {
-                        filters.types.push(obj.name);
-                    });
-                });
-            }
-        }
-
-        /**
-         * Set the province code from the province name then query geosearch for updated results.
-         *
-         * @function setProvince
-         * @param   {String}    value   the province filter value.
-         */
-        function setProvince(value) {
-            geoSearch.getProvinces().then((provinces) => provinces.find((province) => province.name === value))
-                .then((result) => {
-                    geoSearch.setProvince(value !== 'Province' ? result.code : undefined);
-                    queryGeosearch();
-                }
-            );
-        }
-
-        /**
-         * Set the type code from the type name then query geosearch for updated results.
-         *
-         * @function setType
-         * @param   {String}    value   the type filter value.
-         */
-        function setType(value) {
-            geoSearch.getTypes().then((types) => types.find((type) => type.name === value))
-                .then((result) => {
-                    geoSearch.setType(value !== 'Type' ? result.code : undefined);
-                    queryGeosearch();
-                }
-            );
-        }
-
-        /**
-         * Set the extent value then query geosearch for updated results.
-         *
-         * @function setExtent
-         * @param   {String}    value   the extent filter value.
-         */
-        function setExtent(value) {
-            filters.extent.selection = value;
-            geoSearch.setExtent((value) ? 'visible' : 'canada');
-            queryGeosearch();
-        }
-
-        /**
-         * Close geosearch panel and open toc panel.
-         *
-         * @function toggleToc
-         */
-        function toggleToc() {
-            stateManager.setActive({ mainGeosearch: false }, 'mainToc');
-        }
-
-        /**
-         * Toggle geosearch panel. Use debounce function to avoid wierd behaviour when click
-         * repetitively
-         *
-         * @function toggleGeosearch
-         */
-        function toggleGeosearch() {
-            debToggleGeo();
-        }
-
-        /**
-         * Closes geosearch pane and switches to the previous pane if any.
-         * @function closeGeosearch
-         * @private
-         */
-        function closeGeosearch() {
-            // remove class to set back "main panel goes to bottom"
-            mainPanel.removeClass('rv-geosearch-filters');
+            // stateManager is a mess; it needs to be refactored; this won't happen.
 
             // check if another panel was open before opening geosearch
             // if filters is open, toggle mainToc otherwise the last panel
             // if not, only close geosearch
             const panels = stateManager.panelHistory;
-            if (panels.length > 1) {
-                stateManager.togglePanel('mainGeosearch',
-                    (panels.find((item) => { return item === 'filtersFulldata'; }) === undefined) ?
-                        panels[panels.length - 2] : 'mainToc');
+            if (panels.length > 0) {
+                stateManager.state.main.activeSkipOverride = false;
+
+                const toPanel = panels.reverse().find((item) =>
+                    item === 'mainToc' || item === 'mainDetails');
+
+                stateManager.setActive({ [toPanel]: true });
             } else {
-                stateManager.setActive({ mainGeosearch: false });
+                stateManager.setActive({ main: false })
+                    .then(() => {
+                        stateManager.state.main.activeSkipOverride = false;
+                    });
             }
+
+            // remove class to set back "main panel goes to bottom"
+            ref.mainPanel.removeClass('geosearch');
+        }
+
+        /**
+         * Runs geosearch query and returns the results or suggestions.
+         *
+         * @function runQuery
+         * @return {Promise} promise resolving with results (when results are found -  { results: [] }) or suggestions (when results are not found - { suggestions: [] }) or nothing (when search value is not specified - {});
+         */
+        function runQuery() {
+            const requestCount = ++ref.runningRequestCount;
+
+            // skip when the search value is not specified
+            if (!service.searchValue) {
+
+                service.searchResults = [];
+                service.isLoading = false;
+                service.isResultsVisible = false;
+
+                return $q.resolve();
+            }
+
+            // show loading indicator
+            service.isLoading = true;
+
+            return geoSearch.query(`${service.searchValue}*`).then(data => {
+
+                // hide loading indicator
+                service.isLoading = false;
+                service.isResultsVisible = true;
+
+                // discard any old results
+                if (requestCount === ref.runningRequestCount) {
+                    service.searchResults = data.results || [];
+
+                    service.noResultsSearchValue = service.searchResults.length === 0 ? service.searchValue : '';
+                }
+
+                // return data for optional processing further down the promise chain
+                return data;
+            });
+        }
+
+        /**
+         * @function zoomTo
+         * @param {Object} result a search result to zoom to
+         */
+        function zoomTo(result) {
+            geoSearch.zoomSearchExtent(result.bbox, result.position);
         }
     }
 })();
