f3692dc6ecde2fc85fbf01cea964a9cd0ed45aff
Johann Levesque <johann.levesque@canada.ca>
2017-04-21 14:33:51 -0400

    fix(events): add return event object to access it from outside
    Closes #1858

--- src/events.js
+++ src/events.js
@@ -4,15 +4,18 @@
  *
  * @param {esriObject} esriObject which contains the dojo events to be wrapped
  * @param {handlers} handlers is an object which contains all handlers needed
+ *
+ * @return {object} evt contains the events created on the object, keyed by same properties as handlers input
  */
 function wrapEvents(esriObject, handlers) {
+    const evt = {};
     Object.keys(handlers).forEach(ourEventName => {
         // replace camelCase name to dojo event name format
         const dojoName = ourEventName.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 
         // TODO: Validity checking on inputs for dojoName
         // make dojo call
-        esriObject.on(dojoName, (e) => {
+        evt[ourEventName] = esriObject.on(dojoName, (e) => {
 
             // check if needs special handling to point at layer calling event
             const layerEvents = ['update-start', 'update-end', 'error'];
@@ -22,6 +25,8 @@ function wrapEvents(esriObject, handlers) {
             handlers[ourEventName](e);
         });
     });
+
+    return evt;
 }
 
 module.exports = () => {
