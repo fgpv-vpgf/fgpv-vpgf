ebf7e838ac2c4e2e0151c0decfa69b9734ab7b4e
Aly Merchant <aly.merchant@ec.gc.ca>
2017-05-08 10:22:28 -0400

    toScreen passthrough

--- src/map/esriMap.js
+++ src/map/esriMap.js
@@ -13,7 +13,7 @@ function esriMap(esriBundle, geoApi) {
         // TODO when jshint parses instance fields properly we can change this from a property to a field
         get _passthroughBindings () { return [
             'on', 'reorderLayer', 'addLayer', 'disableKeyboardNavigation', 'removeLayer', 'resize', 'reposition',
-            'centerAt', 'setZoom', 'centerAt'
+            'centerAt', 'setZoom', 'centerAt', 'toScreen'
         ]; }
         get _passthroughProperties () { return ['graphicsLayerIds', 'layerIds', 'spatialReference', 'extent']; } // TODO when jshint parses instance fields properly we can change this from a property to a field
 
