5d5a971af5cd9a40149b265860ff785a703976d3
Miles Petrov <milesap@gmail.com>
2017-05-26 07:19:17 -0400

    fix deployment issue and reviewable comments

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,6 @@
 const Merge         = require('webpack-merge');
 const CommonConfig  = require('./webpack.common.js');
 
-
 module.exports = function (env) {
     const config = Merge(CommonConfig(env), {
         devtool: 'cheap-source-map',
