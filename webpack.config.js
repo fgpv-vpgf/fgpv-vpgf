6e314c87d327622ff2f7c1a62583be3ddfd9b3bd
Miles Petrov <milesap@gmail.com>
2017-05-31 10:07:40 -0400

    chore(build): added env.geoLocal
    Replaces geoApi from node_modules with a local copy on disk. Works with
    live reload.
    
    Source maps no longer generate for develop automatically (faster builds). Use env.useMap
    to enable.
    
    Must merge geoApi PR #217 first

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+/* Webpack command line options
+    env.useMap                      -   Enable source maps on develop (increases build time)
+    env.geoLocal                    -   Replaces geoApi from npm node_module with a local geoApi repo folder located by ../geoApi
+    env.geoLocal="path/to/geoApi"   -   same as no argument env.geoLocal but uses provided path to local folder
+    env.inspect                     -   Bundle analysis for use with build debugging and optimization
+*/
 module.exports = function(env = {}) {
     env.inspect = env.inspect ? true : false;  // displays interactive bundle analyzer
     return require(`./webpack.${env.prod ? 'prod' : 'dev'}.js`)(env);
