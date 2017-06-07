71a4ff096cd27303e42c62358ac5557f112b521a
Ahmed <Jahid.Ahmed@ec.gc.ca>
2017-06-07 11:15:46 -0400

    feat(ui): change colorpicker to update preview for colornames

--- src/app/ui/common/minicolor.directive.js
+++ src/app/ui/common/minicolor.directive.js
@@ -18,39 +18,64 @@ function rvMinicolors(graphicsService, $timeout) {
     const directive = {
         require: 'ngModel',
         restrict: 'A',
-        scope: { 'options': '=' },
+        scope: { options: '=' },
         link
     };
 
     return directive;
 
     function link(scope, el, attrbs, ngModel) {
-        el.minicolors(scope.options);
-        el.on('blur', onBlur);
+        el.minicolors(angular.extend(scope.options, {
+            hideSpeed: 0,
+            showSpeed: 0
+        }));
 
+        el.on('focus', onFocus);
+        el.on('blur', onBlur);
+        el.on('keyup', onKeyUp);
+
+        // set initial value of color preview panel
         ngModel.$render = () => {
-            $timeout(() => {
-                const color = ngModel.$viewValue;
-                el.minicolors('value', color);
-            }, 0, false);
+            const color = ngModel.$viewValue;
+            el.minicolors('value', color);
         };
+
+        function onFocus() {
+            el.minicolors('hide');
+        }
 
         function onBlur() {
             let color = ngModel.$viewValue;
+            color = colorHex(color);
+
+            /*
+                need to use ngModel to set the view value because of inconsistency with minicolors replacing
+                first character to a '0' 
+            */
+            ngModel.$setViewValue(color);
+            ngModel.$render();
+        }
+
+        function onKeyUp() {
+            let color = ngModel.$viewValue;
+            color = colorHex(color);
+            el.siblings().filter('.minicolors-swatch').css('background-color', color);
+        }
+
+        function colorHex(color) {
+
+            // remove any whitespace in color name
+            color = color.replace(/\s/g, '');
 
             // check that the input is not / does not contain a hexadecimal value
             if (color && !/\b([0-9A-F]{6}$)\b|\b([0-9A-F]{3}$)\b/i.test(color)) {
-
-                // remove any whitespace in color name
-                color = color.replace(/\s+/g, '');
                 color = nameToHex(color);
             }
 
             // ensure the color has a hash symbol at the start
-            if (color.charAt(0) !== '#')    color = '#' + color;
+            if (color && color.charAt(0) !== '#')    color = '#' + color;
 
-            ngModel.$setViewValue(color);
-            ngModel.$render();
+            return color;
         }
     }
 }
