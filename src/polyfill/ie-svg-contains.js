04e70c177082cf8507be94d16d677c3ef85d5db2
Aly Merchant <aly.merchant@ec.gc.ca>
2017-06-09 13:09:22 -0400

    fix(core): add SVGElement.contains polyfill

---
+++ src/polyfill/ie-svg-contains.js
@@ -0,0 +1,15 @@
+if (!SVGElement.prototype.contains) {
+    SVGElement.prototype.contains = function contains(node) {
+        if (!(0 in arguments)) {
+            throw new TypeError('1 argument is required');
+        }
+
+        do {
+            if (this === node) {
+                return true;
+            }
+        } while (node = node && node.parentNode);
+
+        return false;
+    };
+}
