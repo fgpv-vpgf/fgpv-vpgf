53ad6799dd49218b951bb4ba1d0cbbd14058de21
Miles Petrov <milesap@gmail.com>
2017-06-12 12:11:01 -0400

    feat(test): add initial protractor test support

---
+++ protractor_tests/conf.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['sidePanel-spec.js'],
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 60000
+    },
+    allScriptsTimeout: 60000
+};
