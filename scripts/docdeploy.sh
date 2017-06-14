1ca07901cec8865b145abdae036b5624905b804c
Aly Merchant <aly.merchant@ec.gc.ca>
2017-06-14 09:59:17 -0400

    chore(build): add ssh key setup to docdeploy

--- scripts/docdeploy.sh
+++ scripts/docdeploy.sh
@@ -4,13 +4,19 @@ set -e
 
 if [ "$TRAVIS_REPO_SLUG" == "fgpv-vpgf/geoApi" ] && [ -n "$TRAVIS_TAG" ]; then
     npm run doc
-    # this section assumes the id_rsa key has already been decrypted
-    # devdeploy.sh should run before this script
+
+    openssl aes-256-cbc -k "$PW" -out ~/.ssh/id_rsa -in devkey.enc -d
+    echo -e "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+    chmod 600 ~/.ssh/id_rsa
+    eval `ssh-agent -s`
+    ssh-add ~/.ssh/id_rsa
+
     git clone --depth=1 git@github.com:fgpv-vpgf/geoApi.git -b gh-pages ghdocs
     ls ghdocs
     mkdir -p ghdocs/$TRAVIS_TAG
     rsync -av --delete docbuild/ ghdocs/$TRAVIS_TAG/
     bash ./scripts/make_doc_index.sh ghdocs/ > ghdocs/index.html
+
     cd ghdocs
     git add $TRAVIS_TAG
     git add index.html
