a0a1018f4b4100ec68117b97f035af78b94c445a
JahidAhmed <ahmedjahid@gmail.com>
2017-06-22 09:09:24 -0400

    fix(datatable): loading for dynamic layers

--- src/app/core/constant.service.js
+++ src/app/core/constant.service.js
@@ -59,7 +59,8 @@ function events($rootScope) {
         rvMouseMove: 'rvMouseMove', // Fired when mouse move over the map (use to calculate mouse coordinates)
         rvBasemapChange: 'rvBasemapChange', // Fired when basemap is changed
         rvHighlightDetailsItem: 'rvHighlightDetailsItem',
-        rvGeosearchClose: 'rvGeosearchClose' // Fire when geosearch close
+        rvGeosearchClose: 'rvGeosearchClose', // Fire when geosearch close
+        rvTableReady: 'rvTableReady'
     };
 }
 
