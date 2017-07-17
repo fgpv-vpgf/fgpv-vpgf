b706e08aa132f524a2a809063429a2a4e8f74b42
Miles Petrov <milesap@gmail.com>
2017-07-17 11:04:01 -0400

    fix(datatable): added title to column headings

--- src/app/ui/table/table-default.directive.js
+++ src/app/ui/table/table-default.directive.js
@@ -590,7 +590,7 @@ function rvTableDefault($timeout, $q, stateManager, $compile, geoService, $trans
                     angular.element(tableService.getTable().header()).find('th').each((i, el) => {
                         const title = el.innerHTML;
                         el.innerHTML = '';
-                        el.appendChild(angular.element(`<span data-rv-column="${title}">${title}</span>`)[0]);
+                        el.append(angular.element(`<span title="${title}" data-rv-column="${title}">${title}</span>`)[0]);
                     });
 
                     // fired event to create filters
