7558a7694395dcaf9ee9b00b1ef008b5b6717fac
Miles Petrov <milesap@gmail.com>
2017-07-18 14:01:26 -0400

    chore(build): increase protractor wait time

--- protractor_tests/conf.js
+++ protractor_tests/conf.js
@@ -2,7 +2,7 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['sidePanel-spec.js'],
     jasmineNodeOpts: {
-        defaultTimeoutInterval: 60000
+        defaultTimeoutInterval: 120000
     },
-    allScriptsTimeout: 60000
+    allScriptsTimeout: 120000
 };
