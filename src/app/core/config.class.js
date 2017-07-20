d9f41ce2f36657327127230cd17af1f1ff5f22d9
James Rae <james.rae@canada.ca>
2017-07-20 17:00:56 -0400

    fix(geo): custom overview and bugs

--- src/app/core/config.class.js
+++ src/app/core/config.class.js
@@ -1028,7 +1028,7 @@ function ConfigObjectFactory(Geo, gapiService, common) {
         get name () { return this._name; }
         get id () { return this._id; }
 
-        get overviewUrl () { return this.__overviewUrl; }
+        get overviewUrl () { return this._overviewUrl; }
 
         get extentSet () { return this._extentSet; }
         get lodSet () { return this._lodSet; }
@@ -1063,6 +1063,7 @@ function ConfigObjectFactory(Geo, gapiService, common) {
             return blankBasemap;
         }
 
+        // TODO add overviewUrl object to this?
         get JSON () {
             return {
                 id: this.id,
@@ -1433,15 +1434,18 @@ function ConfigObjectFactory(Geo, gapiService, common) {
 
             this._maximizeButton = source.maximizeButton;
             this._layerType = source.layerType;
+            this._expandFactor = source.expandFactor;
         }
 
         get maximizeButton () { return this._maximizeButton; }
         get layerType () { return this._layerType; }
+        get expandFactor () { return this._expandFactor; }
 
         get JSON() {
             return angular.merge(super.JSON, {
                 maximizeButton: this.maximizeButton,
-                layerType: this.layerType
+                layerType: this.layerType,
+                expandFactor: this.expandFactor
             });
         }
     }
