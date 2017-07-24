9b42fdcb9c7f4312b323e7ea454767936b32af60
Jahid Ahmed <ahmedjahid@gmail.com>
2017-07-24 12:37:40 -0400

    fix(datatable): filter container displayed in settings panel

--- src/app/ui/table/table.service.js
+++ src/app/ui/table/table.service.js
@@ -486,9 +486,10 @@ function tableService(stateManager, geoService, $rootScope, $q, gapiService, deb
         service.filter.isActive = filter.isActive;
         service.filter.isApplied = filter.isApplied;
         service.filter.isMapFiltered = filter.isMapFiltered;
+        service.filter.isOpen = filter.isOpen;
 
         // check if we need to show the filters (need this check when table is created)
-        layoutService.isFiltersVisible = service.filter.isOpen;
+        layoutService.isFiltersVisible = service.isSettingOpen ? true : service.filter.isOpen;
 
         // set layer type to see if we show apply filter button. Only works on feature/dynamic layer
         // user added layer (layer created by value from a feature collection like CSV file) does not support definition expressions and time definitions)
