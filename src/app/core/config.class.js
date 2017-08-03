8d480c8f0fe0c18e9515f2f2ff0854a8bf547fa2
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2017-08-03 14:58:11 -0400

    fix(core): default expand factor of overviewmap

--- src/app/core/config.class.js
+++ src/app/core/config.class.js
@@ -1440,7 +1440,7 @@ function ConfigObjectFactory(Geo, gapiService, common) {
 
             this._maximizeButton = source.maximizeButton;
             this._layerType = source.layerType;
-            this._expandFactor = source.expandFactor;
+            this._expandFactor = source.expandFactor || 2;
         }
 
         get maximizeButton () { return this._maximizeButton; }
