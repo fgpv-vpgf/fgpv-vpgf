54c71b04ec84604ca2455896898f21e01ca2258e
Yung <Barry.Yung@ec.gc.ca>
2017-09-15 15:36:54 -0400

    fix(toc): make sure table config exits

--- src/app/ui/toc/toc.service.js
+++ src/app/ui/toc/toc.service.js
@@ -13,7 +13,7 @@ angular
 
 function tocService($q, $rootScope, $mdToast, $translate, referenceService, common, stateManager, graphicsService,
     geoService, metadataService, errorService, LegendBlock, configService, legendService, layoutService) {
-        
+
     const service = {
         // method called by the options and flags set on the layer item
         actions: {
@@ -53,12 +53,15 @@ function tocService($q, $rootScope, $mdToast, $translate, referenceService, comm
      * @param {LegendBlock} legendBlock legend block to be reloaded
      */
     function reloadLayer(legendBlock) {
+
         // get table configuration and check if static field were used. If so, table can't be remove and flag need to stay
-        const config = configService.getSync.map.layerRecords.find(item =>
+        const tableConfig = configService.getSync.map.layerRecords.find(item =>
             item.config.id === legendBlock.layerRecordId).initialConfig.table;
 
         // update filter flag
-        legendBlock.filter = config.applied;
+        if (tableConfig) {
+            legendBlock.filter = tableConfig.applied;
+        }
 
         stateManager.setActive({ tableFulldata: false } , { sideMetadata: false }, { sideSettings: false });
         legendService.reloadBoundLegendBlocks(legendBlock.layerRecordId);
