1e3a435451fed4017222b95c005b32cf34ebc224
Jahid Ahmed <ahmedjahid@gmail.com>
2017-10-17 14:15:00 -0400

    refactor(wms): implement pr suggestions

--- src/app/ui/settings/settings-content.directive.js
+++ src/app/ui/settings/settings-content.directive.js
@@ -47,6 +47,8 @@ function rvSettingsContent(layerRegistry) {
             self.valueParentBlock = findOpacityParentBlock(self.block);
         }
 
+        self.cachedStyles = self.block.mainProxyWrapper.layerConfig.layerEntries.map(le => le.currentStyle);
+
         /**
          * Finds the closes parent group which enabled and visible opacity control.
          *
