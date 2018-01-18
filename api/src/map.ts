720b01b429c21a4229cd9dbe87de69785e442c9b
Jahid Ahmed <ahmedjahid@gmail.com>
2018-01-18 10:27:50 -0500

    fix(api): layer state changing and other small fixes

--- api/src/map.ts
+++ api/src/map.ts
@@ -38,6 +38,7 @@ export default class Map {
         this.mapDiv = $(mapDiv);
         this._id = this.mapDiv.attr('id') || '';
         this._ui = new UI(this);
+        this._layers = [];  // TODO: need to instantiate LayerGroup when implemented  ?
 
         // config set implies viewer loading via API
         if (config) {
@@ -53,11 +54,9 @@ export default class Map {
             seeder(mapDiv);
             this.mapDiv.attr('is', 'rv-map'); // needed for css styling issues
         }
-
-        this._layers = [];  // need to instantiate LayerGroup when implemented  ?
     }
 
-    get layers(): Array<any> { return this._layers; }  // change type after to LayerGroup when implemented  ?
+    get layers(): Array<any> { return this._layers; }  // TODO: change type after to LayerGroup when implemented  ?
 
     /** Once set, we know the map instance is ready. */
     set fgpMap(fgpMap: Object) {
