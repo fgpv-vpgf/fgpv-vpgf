e49175fddfd8c2ab5ecf4f2c9a143abb7b2594f6
Yung <Barry.Yung@ec.gc.ca>
2018-04-06 14:16:17 -0400

    fixt(intention): add back init execution

--- intention/epsg/epsg.intention.ts
+++ intention/epsg/epsg.intention.ts
@@ -1,10 +1,22 @@
-function lookup(lookup: string | number) {
+/**
+ * @namespace epsg
+ * @description An intention that provides an epsg look up function
+ */
+
+
+/**
+ * @description Lookup a proj4 style projection definition for a given ESPG code.
+ * @function lookup
+ * @param {string|number} code the EPSG code as a string or number
+ * @return {Promise} a Promise resolving to proj4 style definition or null if the definition
+ */
+function lookup(code: string | number) {
     const urnRegex = /urn:ogc:def:crs:EPSG::(\d+)/;
     const epsgRegex = /EPSG:(\d+)/;
-    let matcher = String(lookup).match(urnRegex) || String(lookup).match(epsgRegex) || [];
+    let matcher = String(code).match(urnRegex) || String(code).match(epsgRegex) || [];
 
     if (!matcher || matcher.length < 2) {
-        throw new Error('Invalid lookup string provided.');
+        throw new Error('Invalid code provided.');
     }
 
     return new Promise((resolve, reject) => {
