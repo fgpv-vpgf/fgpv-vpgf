494a62c0289c7c55c5247c5b952baf314857ebc9
ChrisLatRNCan <christian.latouche@canada.ca>
2018-05-29 20:05:38 -0400

    fix(table): table dates filtering
    Before: filter from config of type 'date' not filtering the table.
    Values not loading properly. Confusion on type ('date' vs 'rv-date')
    Filtering results from search, date picker and config not similar.
    After: filter from config of type 'date' filtering the table.
    Filtering results from search, date picker and config are similar.
    
    Closes #2479

--- src/app/ui/table/table-default.directive.js
+++ src/app/ui/table/table-default.directive.js
@@ -117,7 +117,7 @@ function rvTableDefault($timeout, $q, stateManager, $compile, geoService, $trans
             number: {
                 init: () => ({ min: '', max: '', static: false })
             },
-            'rv-date': {
+            date: {
                 init: () => ({ min: null, max: null, static: false })
             }
         };
@@ -177,7 +177,7 @@ function rvTableDefault($timeout, $q, stateManager, $compile, geoService, $trans
             const order = initColumns();
 
             // define pre-deformatting function for date columns
-            $.fn.dataTable.ext.order['rv-date-pre'] = d => parseInt(d.replace(/\D/g, ''));
+            $.fn.dataTable.ext.order['date-pre'] = d => parseInt(d.replace(/\D/g, ''));
 
             // Reset datatable to initial state if all sorts are removed
             // See: https://github.com/fgpv-vpgf/fgpv-vpgf/issues/2119
@@ -340,7 +340,7 @@ function rvTableDefault($timeout, $q, stateManager, $compile, geoService, $trans
                                 displayData.rows.forEach(r => { r[field.name] = $filter('dateTimeZone')(r[field.name]) });
                                 const width = getColumnWidth(column.title, 0, 400, 375);
                                 column.width = `${width}px`;
-                                column.type = 'rv-date';
+                                column.type = 'date';
                             } else {
                                 const width = getColumnWidth(column.title, 0, 250, 120);
                                 column.width = `${width}px`;
@@ -584,12 +584,24 @@ function rvTableDefault($timeout, $q, stateManager, $compile, geoService, $trans
                         if (column.type === 'string' || column.type === 'selector') {
                             column.filter.value = config.filter.value;
                         } else if (column.type === 'number') {
+                            // expect string "numberMin,numberMax"
                             const values = config.filter.value.split(',');
                             column.filter.min = values[0];
                             column.filter.max = values[1];
-                        } else {
-                            column.filter.min = config.filter.value.min;
-                            column.filter.max = config.filter.value.max;
+                        } else if(column.type === 'date') {
+                            // expect string "dateMin,dateMax"
+                            // dates should be in format YYYY-MM-DD
+                            const values = config.filter.value.split(',');
+
+                            const timeMin = $filter('dateTimeZone')(values[0], '');
+                            if (timeMin !== 'Invalid date') {
+                                column.filter.min = new Date(timeMin);
+                            }
+                            
+                            const timeMax = $filter('dateTimeZone')(values[1], '');
+                            if (timeMax !== 'Invalid date') {
+                                column.filter.max = new Date(timeMax);
+                            }
                         }
                     }
 
