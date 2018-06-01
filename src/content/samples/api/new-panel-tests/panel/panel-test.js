66c2ad51257d4aa7657fd28331b933504c861e73
Vellanki <Shruti.Vellanki@ec.gc.ca>
2018-06-01 11:54:17 -0400

    feat(api): re-added most recent changes from new-panel-api

---
+++ src/content/samples/api/new-panel-tests/panel/panel-test.js
@@ -0,0 +1,96 @@
+$('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', './panel.css'));
+$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '../../../rv-styles.css') );
+
+$.getScript('../../../rv-main.js', function () {
+
+    //first append map to body
+    $('body').append(`
+        <div id="fgpmap" style="height: 700px;" rv-langs='["en-CA", "fr-CA"]' rv-service-endpoint="http://section917.cloudapp.net:8000/" rv-extensions="../hello-world.js"></div>
+    `);
+
+    //this is the mapInstance
+    const mapInstance = new RZ.Map(document.getElementById('fgpmap'), '../../config.rcs.[lang].json');
+
+    //once map is added
+    RZ.mapAdded.subscribe(mapi => {
+
+        //SETUP:
+        let text = "Title";
+        // let text = "<p>Text PanelElem</p><h2></h2>"; //multiple top level elements --> throws exception successfully 
+        let htmlInput = $("#coolInput");
+        let panelElem1 = new RZ.PanelElem(text);
+        let panelElem2 = new RZ.PanelElem($("<div>Contents:</div>"));
+        let panelElem3 = new RZ.PanelElem(htmlInput);
+        let panelElem4 = new RZ.PanelElem($('<p>Controls:</p>'))
+        let imgElem = new RZ.PanelElem($("#coolImg"));
+        let closeBtn = new RZ.PanelElem('x');
+
+        let btn = new RZ.Btn();
+        btn.text = "Btn.";
+        $(btn.element).click(function () {
+            alert('Btn element clicked!')
+        });
+
+
+        let btn2 = new RZ.Btn();
+        btn2.icon = document.getElementById('green-rect');
+
+        //Creating a panelElem to be set as contents. User defines line breaks/formatting etc. (this is like dropping a document fragment)
+        $(panelElem2.element).append($("<br>"));
+        $(panelElem2.element).append($("<br>"));
+        $(panelElem2.element).append(btn.element);
+        $(panelElem2.element).append(btn2.element);
+        $(panelElem2.element).append($("<br>"));
+        $(panelElem2.element).append($("<br>"));
+        $(panelElem2.element).append(imgElem.element);
+        
+
+        
+        let lightyear = new RZ.Panel("Buzz Lightyear");
+        mapi.createPanel('Buzz Lightyear', lightyear);
+
+        let woody = new RZ.Panel("Woody");
+        mapi.createPanel('Woody', woody);
+
+        console.log(lightyear.availableSpaces()); //or console.log(mapi.panelRegistry);
+
+        //PANEL TESTS:         
+
+        //line breaks need to be added in by the user        
+        lightyear.controls = [closeBtn, new RZ.PanelElem('|'), new RZ.PanelElem('T') , panelElem1, new RZ.PanelElem($('<br>')), panelElem4, panelElem3];
+        
+        //console.log(lightyear.controls);        
+        lightyear.content = panelElem2;
+        console.log(lightyear.content);
+
+
+        //console.log("Panel ID: "+ lightyear.id);        
+        //lightyear.coverable = true;
+        //console.log(lightyear.coverable);
+        //console.log(lightyear.element);
+        //lightyear.setPosition(2,3,4,25); //out of bounds of grid
+        //lightyear.setMinPosition(2,3,3,4); 
+        //lightyear.setMinPosition(-1, 3, 4, 5) //out of bounds of grid (also of the set position)
+        lightyear.setPosition(5, 5, 9, 18);
+        lightyear.setMinPosition(5,5,7,7); //if not set, all -1 should be set because of default minimum position 
+
+        woody.setPosition (10,1, 15, 4);
+
+        lightyear.open();
+        woody.open();
+        //console.log(mapi.panelRegistry); 
+        //console.log
+
+        //conflict detected tests
+
+        //PANEL OBSERVABLES FIRE:
+        //rowsChanged
+        //colsChanged
+        //positionChanged
+        //visibilityChanged
+
+
+        console.log("panelElem1 ID: " + panelElem1.id);
+
+    });
+});
