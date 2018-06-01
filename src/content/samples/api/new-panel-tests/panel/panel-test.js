c16ccb9fc8e116b291d263b6872c098cad887721
Vellanki <Shruti.Vellanki@ec.gc.ca>
2018-06-01 12:35:35 -0400

    feat(api): panel-test.js can now load css stylesheet

--- src/content/samples/api/new-panel-tests/panel/panel-test.js
+++ src/content/samples/api/new-panel-tests/panel/panel-test.js
@@ -1,15 +1,15 @@
 $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', './panel.css'));
-$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '../../../rv-styles.css') );
+$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '../../../../rv-styles.css') );
 
-$.getScript('../../../rv-main.js', function () {
+$.getScript('../../../../rv-main.js', function () {
 
     //first append map to body
     $('body').append(`
-        <div id="fgpmap" style="height: 700px;" rv-langs='["en-CA", "fr-CA"]' rv-service-endpoint="http://section917.cloudapp.net:8000/" rv-extensions="../hello-world.js"></div>
+        <div id="fgpmap" style="height: 700px;" rv-langs='["en-CA", "fr-CA"]' rv-service-endpoint="http://section917.cloudapp.net:8000/" rv-extensions="../../hello-world.js"></div>
     `);
 
     //this is the mapInstance
-    const mapInstance = new RZ.Map(document.getElementById('fgpmap'), '../../config.rcs.[lang].json');
+    const mapInstance = new RZ.Map(document.getElementById('fgpmap'), '../../../config.rcs.[lang].json');
 
     //once map is added
     RZ.mapAdded.subscribe(mapi => {
