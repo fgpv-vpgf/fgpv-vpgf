3f6a4f75ed4c8c4257ab9da87a32a9f53ee90890
ShrutiVellanki <shruti.vellanki@canada.ca>
2018-06-03 22:31:21 -0700

    feat(api): started working on min position checks

--- src/content/samples/api/new-panel-tests/panel/panel-test.js
+++ src/content/samples/api/new-panel-tests/panel/panel-test.js
@@ -1,5 +1,5 @@
 $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', './panel.css'));
-$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '../../../../rv-styles.css') );
+$('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', '../../../../rv-styles.css'));
 
 $.getScript('../../../../rv-main.js', function () {
 
@@ -43,9 +43,9 @@ $.getScript('../../../../rv-main.js', function () {
         $(panelElem2.element).append($("<br>"));
         $(panelElem2.element).append($("<br>"));
         $(panelElem2.element).append(imgElem.element);
-        
 
-        
+
+
         let lightyear = new RZ.Panel("Buzz Lightyear");
         mapi.createPanel('Buzz Lightyear', lightyear);
 
@@ -57,8 +57,8 @@ $.getScript('../../../../rv-main.js', function () {
         //PANEL TESTS:         
 
         //line breaks need to be added in by the user        
-        lightyear.controls = [closeBtn, new RZ.PanelElem('|'), new RZ.PanelElem('T') , panelElem1, new RZ.PanelElem($('<br>')), panelElem4, panelElem3];
-        
+        lightyear.controls = [closeBtn, new RZ.PanelElem('|'), new RZ.PanelElem('T'), panelElem1, new RZ.PanelElem($('<br>')), panelElem4, panelElem3];
+
         //console.log(lightyear.controls);        
         lightyear.content = panelElem2;
         console.log(lightyear.content);
@@ -69,10 +69,10 @@ $.getScript('../../../../rv-main.js', function () {
         //lightyear.setPosition(2,3,4,25); //out of bounds of grid
         //lightyear.setMinPosition(2,3,3,4); 
         //lightyear.setMinPosition(-1, 3, 4, 5) //out of bounds of grid (also of the set position)
-        //lightyear.setPosition(5, 5, 9, 18);
-        //lightyear.setMinPosition(5,5,7,7); //if not set, all -1 should be set because of default minimum position 
+        lightyear.setPosition(5, 1, 9, 18);
 
-        woody.setPosition (10,1, 15, 4);
+        lightyear.setMinPosition(5, 5, 7, 7); //if not set, all -1 should be set because of default minimum position 
+        woody.setPosition(10, 1, 15, 4);
 
         lightyear.open();
         woody.open();
@@ -93,17 +93,49 @@ $.getScript('../../../../rv-main.js', function () {
         /**
          * TEST CASES: 
          * lightyear.open() before lightyear.setPosition (expect console error)
-         * Set min position out of bounds of original position
          * Set position out of bounds of grid
          * Set position with invalid values (lower than zero)
          * Set invalid positions (where topLeft values are greater than bottomRight values)
-         * Set panel's min position within another panel's position (should throw an error that panel can't be set)
+         * Open panel whose min position is within another panel's position (should throw error)
+         * Adjust panel to min position if its non-min position is within another panel's position
          * If panel's non-min position is within another panel's territory (doesn't matter if other panel's territory is at min or not)
          * - If panel can shrink out of it then it should
          * Does available spaces change everytime panel opens/closes? (not when position it set or changed without open/closing)
          * Does available spaces change everytime panel is added/deleted from map class?
          * Error if panel registry tries to be updated without map instance there (implicitly done)
          * Error if user tries to open or close panel whose position is not set, and whose map isn't created
+         * Error if min position is a not a subset of position 
+         * Error if setMinPosition is called before setPosition
+         * Error if available spaces is called without a map object existing
+         * PanelElem control divider shortcut, close button shortcut
+         * 
+         * 
+         * PANEL METHOD TESTS: 
+         *  open(): void;
+         *  close(): void;
+         *  set controls(elems: PanelElem[]): void;
+         *  get controls(): PanelElem[];
+         *  get content(): PanelElem;
+         *  set content(item: PanelElem): void;
+         *  get id(): string;
+         *  get element(): jQuery<HTMLElement>;
+         *  setPosition(topLeft: number,bottomRight: number): void;
+            setMinPosition(topLeft: number,bottomRight: number): void;
+            widthChanged: Observable<number>; // the width in pixels of a grid square
+            heightChanged: Observable<number>; // the height in pixels of a grid square
+            positionChanged: Observable<number, number>; // topLeft, bottomRight
+            opening: Observable;
+            closing: Observable;
+            static availableSpaces(width: number, height: number): number[][];
+            availableSpaces(): number[][];
+            get element(): jQuery<HTMLElement>;
+
+            PANELELEM: 
+            *get element
+
+            BTN:
+            set icon(svg: SVG): void;
+            set text(txt: string): void;
          * 
          */
 
