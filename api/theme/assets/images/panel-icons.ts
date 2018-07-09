71d6360bb1ea620d250743c1dc301daa200b5e0e
Miles Petrov <Miles.Petrov@ec.gc.ca>
2018-07-09 13:08:03 -0400

    change grid system to pixel based position system, part 1 - initial pass

--- api/theme/assets/images/panel-icons.ts
+++
@@ -1,3 +0,0 @@
-export const minusSVG = $.parseHTML('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#ffffff" d="M19,13H5V11H19V13Z"/></svg>')[0];
-export const plusSVG = $.parseHTML('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#ffffff" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg>')[0];
-export const closeSVG = $.parseHTML('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#ffffff" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"/></svg>')[0];
