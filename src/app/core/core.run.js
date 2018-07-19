aae326602a4a9b885e22265f64c250f316419f12
Vellanki <Shruti.Vellanki@ec.gc.ca>
2018-07-19 11:24:20 -0400

    feat(extension): fixes to config, schema, and new extension

--- src/app/core/core.run.js
+++ src/app/core/core.run.js
@@ -254,6 +254,11 @@ function apiBlock($rootScope, globalRegistry, geoService, configService, events,
         configService.getSync.map.instance.centerAt(zoomPoint);
     }
 
+    //wire in a hook to get api for a map instance
+    events.$on(events.rvMapLoaded, () => {
+        configService.getSync.map.instance.getApi = () => service
+    });
+
     /**
      * Set extent of the map.
      *
