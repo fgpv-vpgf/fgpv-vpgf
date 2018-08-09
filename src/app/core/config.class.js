b3dbe0d91d106e0d690b64cb32a1f06a1f91dc8c
Miles Petrov <miles@petrovs.ca>
2018-08-09 14:12:23 -0400

    Remove areaOfInterest plugin left-overs

--- src/app/core/config.class.js
+++ src/app/core/config.class.js
@@ -1705,12 +1705,6 @@ function ConfigObjectFactory(Geo, gapiService, common, events, $rootScope) {
         }
     }
 
-    class AreaOfInterestComponent extends ComponentBase {
-        constructor (source) {
-            super(source);
-        }
-    }
-
     class MouseInfoComponent extends ComponentBase {
         constructor (source) {
             super(source);
@@ -1797,7 +1791,6 @@ function ConfigObjectFactory(Geo, gapiService, common, events, $rootScope) {
             this._source = componentsSource;
 
             this._geoSearch = new GeoSearchComponent(componentsSource.geoSearch);
-            this._areaOfInterest = new AreaOfInterestComponent(componentsSource.areaOfInterest);
             this._mouseInfo = new MouseInfoComponent(componentsSource.mouseInfo);
             this._northArrow = new NorthArrowComponent(componentsSource.northArrow);
             this._overviewMap = new OverviewMapComponent(componentsSource.overviewMap);
@@ -1806,7 +1799,6 @@ function ConfigObjectFactory(Geo, gapiService, common, events, $rootScope) {
         }
 
         get geoSearch () { return this._geoSearch; }
-        get areaOfInterest () { return this._areaOfInterest; }
         get mouseInfo () { return this._mouseInfo; }
         get northArrow () { return this._northArrow; }
         get overviewMap () { return this._overviewMap; }
@@ -1816,7 +1808,6 @@ function ConfigObjectFactory(Geo, gapiService, common, events, $rootScope) {
         get JSON() {
             return {
                 geoSearch: this.geoSearch.JSON,
-                areaOfInterest: this.areaOfInterest.JSON,
                 mouseInfo: this.mouseInfo.JSON,
                 northArrow: this.northArrow.JSON,
                 overviewMap: this.overviewMap.JSON,
