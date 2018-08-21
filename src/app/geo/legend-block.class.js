ec6d4e46c8fb4982da808015e51e035fd90661fd
Jahid Ahmed <ahmedjahid@gmail.com>
2018-08-21 11:32:47 -0400

    feat(LegendGroup): implement groups for legend API

--- src/app/geo/legend-block.class.js
+++ src/app/geo/legend-block.class.js
@@ -81,6 +81,7 @@ function LegendBlockFactory(
                 this.visibility = this._layerConfig.state.visibility;
                 this.query = this._layerConfig.state.query;
 
+                // need to update legend visibility and opacity as well
                 this._updateApiLayerVisibility(this);
                 this._updateApiLayerOpacity(this);
                 this._updateApiLayerQueryable(this);
@@ -223,6 +224,7 @@ function LegendBlockFactory(
             // store opacity value in the layer config; will be used by full state restore
             this._layerConfig.state.opacity = value;
 
+            // need to update legend visibility and opacity as well
             this._updateApiLayerOpacity(this);
         }
         set visibility(value) {
@@ -240,6 +242,7 @@ function LegendBlockFactory(
             // store visibility value in the layer config; will be used by full state restore
             this._layerConfig.state.visibility = value;
 
+            // need to update legend visibility and opacity as well
             this._updateApiLayerVisibility(this);
         }
         set query(value) {
@@ -1381,6 +1384,7 @@ function LegendBlockFactory(
         }
         set expanded(value = !this.expanded) {
             this._expanded = value;
+            $rootScope.$applyAsync();
         }
 
         get entries() {
