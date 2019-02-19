cb0395f6896e90102265f64f8d7a3ad6d116eee8
Miles Petrov <miles.petrov@canada.ca>
2019-02-19 10:24:38 -0500

    chore(build): fix travis doc deploy error

--- scripts/travis/builddocs.sh
+++ scripts/travis/builddocs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$mProc" == "dev" && ("$TRAVIS_BRANCH" == "develop" ] || "$TRAVIS_BRANCH" == "master" || [ ${TRAVIS_BRANCH:0:1} == "v")]; then
+if ((${mProc} == "dev")) && ((${TRAVIS_BRANCH} == "develop" || ${TRAVIS_BRANCH} == "master" || ${TRAVIS_BRANCH:0:1} == "v")); then
     npm run docs
     mv ./docs "./$TRAVIS_BRANCH"
     rsync -e 'ssh -i /tmp/docs_rsa' -r --delete-after --quiet "./$TRAVIS_BRANCH" "milesap@fgpv.org:/disk/static/docs/$TRAVIS_REPO_SLUG"
