20f846da0f253248da4e91a7449a971ac3f88ff0
Miles Petrov <miles.petrov@canada.ca>
2019-02-25 12:43:36 -0500

    fix(cleanup): update build files + type data compiler

--- scripts/typeCompiler.js
+++ scripts/typeCompiler.js
@@ -3,18 +3,20 @@ const fs = require('fs');
 const types = {
     "en": {
         FSA: "Forward Sortation Area",
-        NTS: "National Topographic System"
+        NTS: "National Topographic System",
+        COORD: "Latitude/Longitude"
     },
     "fr": {
         FSA: "Région De Tri D'Acheminement",
-        NTS: "Système National De Référence Cartographique"
+        NTS: "Système National De Référence Cartographique",
+        COORD: "Latitude/Longitude"
     }
 };
 
 function fetchConsise(lang) {
     return fetch(`https://geogratis.gc.ca/services/geoname/${lang}/codes/concise.json`)
-    .then(res => res.json())
-    .then(json => json.definitions.forEach(type => types[lang][type.code] = type.term));
+        .then(res => res.json())
+        .then(json => json.definitions.forEach(type => types[lang][type.code] = type.term));
 }
 
 module.exports = Promise.all([fetchConsise('en'), fetchConsise('fr')]).then(() => {
