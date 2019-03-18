6c9f579b86d405a49be5ca3c60c876f8181acd3f
Vellanki <Shruti.Vellanki@ec.gc.ca>
2019-03-18 16:49:18 -0400

    fix(enhancedTable): add loader screen, optional digest param

--- api/src/panel/button.close.ts
+++ api/src/panel/button.close.ts
@@ -3,14 +3,14 @@ import Button from './button';
 
 export default class CloseButton extends Button {
 
-    constructor(panel: Panel) {
-        super(panel, `<md-icon md-svg-src="navigation:close"></md-icon>`, `{{ 'api.panel.close' | translate }}`);
+    constructor(panel: Panel, digest: boolean) {
+        super(panel, digest, `<md-icon md-svg-src="navigation:close"></md-icon>`, `{{ 'api.panel.close' | translate }}`);
         this.elem.addClass('md-icon-button');
         this.elem.removeClass('md-raised');
 
         // close the panel when a user clicks on the button
         this.elem.on('click', () => {
-            panel.close({closingCode: CLOSING_CODES.CLOSEBTN});
+            panel.close({ closingCode: CLOSING_CODES.CLOSEBTN });
         });
     }
 }
