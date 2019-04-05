b4262ff80a94a5d30b51d8eb7764f4426ec65fd6
Miles Petrov <miles.petrov@canada.ca>
2019-04-05 12:41:41 -0400

    0.0.16-0

--- bin/build/travis/dist-build.sh
+++ bin/build/travis/dist-build.sh
@@ -6,11 +6,7 @@ if [ "$TRAVIS_BRANCH" == "develop" ] || "$TRAVIS_BRANCH" == "master" || [ ${TRAV
     echo "Building distribution for latest push to develop or tag."
     npm run build
 
-    # Copy build files into a "plugins" folder which is included with npm publish
-    mkdir plugins
-    for file in {dist/**/*.js,dist/**/*.css}; do cp "$file" plugins/$(basename "${file}");done
-
-    mv ./dist "./$TRAVIS_BRANCH"
+    cp -pr ./dist "./$TRAVIS_BRANCH"
     rsync -e 'ssh -i /tmp/cloud_rsa' -r --delete-after --quiet "./$TRAVIS_BRANCH" "milesap@fgpv.org:/disk/static/builds/$TRAVIS_REPO_SLUG"
     rm -rf "./$TRAVIS_BRANCH"
 
