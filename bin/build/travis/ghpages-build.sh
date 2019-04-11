9d43159ce8b49bcd0f4e014ad168c88084971915
Dane Thomas <8885149+dane-thomas@users.noreply.github.com>
2019-04-11 14:07:39 -0400

    fix(build): fix RAMP source js and css urls (#74)
    * fix(build): fix RAMP source js and css urls

--- bin/build/travis/ghpages-build.sh
+++ bin/build/travis/ghpages-build.sh
@@ -10,5 +10,10 @@ fi
 
 mkdir -p page_files/$TRAVIS_COMMIT
 npm run build
+# Replace all /rv-main.js and /rv-styles.css with: /plugins/rv-main.js|rv-styles.css
+find . -path ./node_modules -prune -o -name '*.html' | while read line; do
+    sed -i -e "s+/rv-main.js+/plugins/$TRAVIS_COMMIT/rv-main.js+g" "$line"
+    sed -i -e "s+/rv-styles.css+/plugins/$TRAVIS_COMMIT/rv-styles.css+g" "$line"
+done
 mv dist/* page_files/$TRAVIS_COMMIT
 echo "" > page_files/index.html
