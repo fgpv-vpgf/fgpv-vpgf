dbf46a6351b1c98556d0f922513c56cdd9dbb681
Spencer Wahl <spencer.wahl2@canada.ca>
2019-05-06 11:05:13 -0400

    fix(build): fix plugin paths

--- scripts/pluginSamples.sh
+++ scripts/pluginSamples.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-for file in node_modules/@fgpv/rv-plugins/dist/**/*; do
-    nosamplefile=${file//samples\//}
-    DIR=$(dirname "src/content/samples/plugins${nosamplefile#node_modules/@fgpv/rv-plugins/dist}")
-    mkdir -p "$DIR"
-    cp -r "./$file" "src/content/samples/plugins${nosamplefile#node_modules/@fgpv/rv-plugins/dist}"
-    # Replace all /rv-main.js and /rv-styles.css with: ../../../../rv-main.js|rv-styles.css
-    find "src/content/samples/plugins${nosamplefile#node_modules/@fgpv/rv-plugins/dist}" -name '*.html' | while read line; do
-        sed -i -e "s+/rv-main.js+../../../../rv-main.js+g" "$line"
-        sed -i -e "s+/rv-styles.css+../../../../rv-styles.css+g" "$line"
-        sed -i -e "s+/legacy-api.js+../../../../legacy-api.js+g" "$line"
-    done
-    if [ ${file: -3} == ".js" ] || [ ${file: -4} == ".css" ]; then
-        DIR=$(dirname "build/plugins${file#node_modules/@fgpv/rv-plugins/dist}")
-        mkdir -p "$DIR"
-        cp -r "./$file" "build/plugins${file#node_modules/@fgpv/rv-plugins/dist}"
+for dir in node_modules/@fgpv/rv-plugins/dist/*/; do
+    # check if last folder is samples, if it is skip..
+    # samples should end up in their parents copy, not on their own
+    if [[ ${dir#$(dirname $dir)} != '/samples/' ]]; then
+        cp -R --remove-destination ${dir} "src/content/samples/plugins/"
     fi
+    # Replace all /rv-main.js and /rv-styles.css with: ../../../../rv-main.js|rv-styles.css
+    find "src/content/samples/plugins" -name '*.html' | while read line; do
+        sed -i -e "s+\"/rv-main.js\"+\"../../../../rv-main.js\"+g" "$line"
+        sed -i -e "s+\"/rv-styles.css\"+\"../../../../rv-styles.css\"+g" "$line"
+        sed -i -e "s+\"/legacy-api.js\"+\"../../../../legacy-api.js\"+g" "$line"
+    done
+    #if [ ${file: -3} == ".js" ] || [ ${file: -4} == ".css" ]; then
+    #    DIR=$(dirname "build/plugins${file#node_modules/@fgpv/rv-plugins/dist}")
+    #    mkdir -p "$DIR"
+    #    cp -r "./$file" "build/plugins${file#node_modules/@fgpv/rv-plugins/dist}"
+    #fi
 done
