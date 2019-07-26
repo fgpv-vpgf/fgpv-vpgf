671ac57b32d5e8ded25024df8720eae163873f64
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2019-07-26 11:53:51 -0400

    core(export): remove sample plugin code

--- src/app/ui/export/export.service.js
+++ src/app/ui/export/export.service.js
@@ -5,6 +5,8 @@ const simpleTemplate = require('./export-simple.html');
 const EXPORT_IMAGE_GUTTER = 20; // padding around the export image
 const EXPORT_CLASS = '.rv-export';
 
+/* global RAMP */
+
 /**
  *
  * @name exportService
@@ -19,7 +21,17 @@ const EXPORT_CLASS = '.rv-export';
  */
 angular.module('app.ui').service('exportService', exportService);
 
-function exportService($rootScope, $mdDialog, $mdToast, referenceService, configService, events, appInfo, exportGenerators, exportSizesService) {
+function exportService(
+    $rootScope,
+    $mdDialog,
+    $mdToast,
+    referenceService,
+    configService,
+    events,
+    appInfo,
+    exportGenerators,
+    exportSizesService
+) {
     const service = {
         open,
         close
@@ -33,11 +45,10 @@ function exportService($rootScope, $mdDialog, $mdToast, referenceService, config
             service.open(null, fileType);
         };
 
-
         exportSizesService.update();
 
-        // expose the exoprt generators on the map instance which exposes it on the API
-        configService.getSync.map.instance.exportGenerators  = exportGenerators.allGenerators;
+        // expose the export generators on the map instance which exposes it on the API
+        configService.getSync.map.instance.exportGenerators = exportGenerators.allGenerators;
     });
 
     let mApi = null;
@@ -148,136 +159,36 @@ function exportService($rootScope, $mdDialog, $mdToast, referenceService, config
         exportComponentsService.init().then(() => {
             // if an export plugin is present, use it to generate export graphics
             if (appInfo.features.export) {
-                console.log(appInfo.features.export);
+                console.log('appInfo.features.export', appInfo.features.export);
 
-                // an export plugin should return a collection of promises each resolving with with a graphic and its offset
+                // An export plugin should return a collection of promises each resolving with with a graphic and its offset
                 // { graphic: <canvas>, offset: [<left>, <top>] }[]
-                // the first graphic is considered to be the base graphic and its offset should be [0,0]
-                // all other graphics will be offset relative to the base graphic
-                // when all promises have resolved, export is considered to be generated
-                // if any of the promises fail, the export is considered to have failed and a standard error message will be displayed
-
-                // CURRENTLY COMMENTED OUT WIP. Code modified though to use generators exposed on API
-                /*
-                /// PLUGIN CODE STARTS
-                const promises = [];
-
-                // create a base image and colour it white
-                const baseImage = graphicsService.createCanvas(
-                    self.exportSizes.selectedOption.width,
-                    self.exportSizes.selectedOption.height
-                );
-                const baseImageCtx = baseImage.getContext('2d');
-                baseImageCtx.fillStyle = '#ffffff';
-                baseImageCtx.fillRect(0, 0, baseImage.width, baseImage.height);
-
-                // create underlying base canvas
-                promises.push(
-                    Promise.resolve({
-                        graphic: baseImage
-                    })
-                );
-
+                // - the first graphic is considered to be the base graphic and its offset should be [0,0]
+                // - all other graphics will be offset relative to the base graphic
+                // - when all promises have resolved, export is considered to be generated
+                // - if any of the promises fail, the export is considered to have failed and a standard error message will be displayed
                 //
-                const mapImageSize = {
-                    width: self.exportSizes.selectedOption.width * 0.8 - 20,
-                    height: self.exportSizes.selectedOption.height - 20
-                };
+                // The plugin is free to rearrange `legendBlocks` as it sees fit as long as its structure remains valid.
 
-                const sourceX = (self.exportSizes.selectedOption.width - mapImageSize.width) / 2;
-                const sourceY = (self.exportSizes.selectedOption.height - mapImageSize.height) / 2;
-
-                // svg export graphic needs to be generated first because generating a server-side map image hides svg layers (unless using local printing)
-                // TODO: prevent map generators from accepting export sizes
-                const apiGenerators = mApi.exportGenerators;
-
-                const pointsImage = apiGenerators.mapSVG().then(data => {
-                    const canvas = graphicsService.createCanvas(mapImageSize.width, mapImageSize.height);
-
-                    // crop the map image returned by the generator to fit into the layout
-                    // https://www.html5canvastutorials.com/tutorials/html5-canvas-image-crop/
-                    canvas
-                        .getContext('2d')
-                        .drawImage(
-                            data.graphic,
-                            sourceX,
-                            sourceY,
-                            mapImageSize.width,
-                            mapImageSize.height,
-                            0,
-                            0,
-                            mapImageSize.width,
-                            mapImageSize.height
-                        );
-
-                    return { graphic: canvas, offset: [10, 10] };
-                });
-                const mapImage = apiGenerators.mapImage().then(data => {
-                    const canvas = graphicsService.createCanvas(mapImageSize.width, mapImageSize.height, '#bfe8fe');
-
-                    // crop the map image returned by the generator to fit into the layout
-                    // https://www.html5canvastutorials.com/tutorials/html5-canvas-image-crop/
-                    canvas
-                        .getContext('2d')
-                        .drawImage(
-                            data.graphic,
-                            sourceX,
-                            sourceY,
-                            mapImageSize.width,
-                            mapImageSize.height,
-                            0,
-                            0,
-                            mapImageSize.width,
-                            mapImageSize.height
-                        );
-
-                    return { graphic: canvas, offset: [10, 10] };
-                });
-
-                const northArrowImage = apiGenerators.northArrow().then(data => ({
-                    graphic: data.graphic,
-                    offset: [40, 20]
-                }));
-
-                const scaleBarImage = apiGenerators.scaleBar().then(data => ({
-                    graphic: data.graphic,
-                    offset: [mapImageSize.width - 10 - 120, mapImageSize.height - 50 - 10]
-                }));
-
-                // we can pass in a modified copy of the legendBlocks if needed, in order to include/exclude certain layers from legend generation
                 // a copy of the legendBlock structure will be passed to export plugins in order to modify as required
                 // TODO: figure out best way to make copy of legendBlocks object. possibly: $.extend({}, configService.getSync.map.legendBlocks), angular.copy(configService.getSync.map.legendBlocks), ... other
-                const legendImage = apiGenerators.legend({
-                    columnWidth: self.exportSizes.selectedOption.width * 0.2 - 20 - 10,
-                    width: self.exportSizes.selectedOption.width * 0.2 - 20 - 10,
-                    height: mapImageSize.height
-                }).then(data => ({
-                    graphic: data.graphic,
-                    offset: [mapImageSize.width + 30, 10]
-                }));
-
-                const titleImage = apiGenerators.title(
-                    `<span style="font-size: 35px; padding: 8px 14px; display: block; text-align: center;"><b>Interesting Fact</b> | Atomic Engineering Lab is out of <i>Cake</i></span>`
-                ).then(data => ({
-                    graphic: data.graphic,
-                    offset: [mapImageSize.width - 10 - data.graphic.width, 10 + 20]
-                }));
-
-                promises.push(mapImage, pointsImage, northArrowImage, scaleBarImage, legendImage, titleImage);
-
-                /// PLUGIN CODE ENDS
+                // NOTE: `angular.copy()` doesn't seem to work properly
+                // const promises = standIn({
+                const promises = appInfo.features.export.generateExportStack({
+                    legendBlocks: configService.getSync.map.legendBlocks,
+                    mapSize: self.exportSizes.selectedOption
+                });
 
                 // RAMP-side; accepts a list of promises from the plugin
                 promises.map((pr, index) =>
                     pr.then(component => {
-                        self.exportPlugin.components[index] = { offset: [0, 0], ...component };
+                        self.exportPlugin.components[index] = { offset: [0, 0], ...component }; // apply the default offset of 0,0
                         $rootScope.$applyAsync();
                     })
                 );
 
                 // wait for all promises returned by the export plugin are resolved
                 Promise.all(promises).then(() => (self.exportPlugin.isGenerating = false));
-                */
 
                 return;
             }
