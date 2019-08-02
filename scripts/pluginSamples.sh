e38dd4a797e3c62335619fe00ccace58b43c5666
Miles Petrov <miles.petrov@canada.ca>
2019-08-02 08:08:53 -0700

    chore(build): fix plugin samples

--- scripts/pluginSamples.sh
+++ scripts/pluginSamples.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-for dir in node_modules/@fgpv/rv-plugins/dist/*/; do
-    # check if last folder is samples, if it is skip..
-    # samples should end up in their parents copy, not on their own
-    if [[ ${dir#$(dirname $dir)} != '/samples/' ]]; then
-        cp -R --remove-destination ${dir} "src/content/samples/plugins/"
-    fi
+if [ -d "node_modules/@fgpv/rv-plugins/dist" ]; then
+    cp -R node_modules/@fgpv/rv-plugins/dist/*/ "src/content/samples/plugins/"
+
     # Replace all /rv-main.js and /rv-styles.css with: ../../../../rv-main.js|rv-styles.css
     find "src/content/samples/plugins" -name '*.html' | while read line; do
         sed -i -e "s+\"/rv-main.js\"+\"../../../../rv-main.js\"+g" "$line"
         sed -i -e "s+\"/rv-styles.css\"+\"../../../../rv-styles.css\"+g" "$line"
         sed -i -e "s+\"/legacy-api.js\"+\"../../../../legacy-api.js\"+g" "$line"
     done
-done
+fi
