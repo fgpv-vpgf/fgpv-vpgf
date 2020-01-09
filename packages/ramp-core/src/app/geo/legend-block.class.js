57bdc3c38dc08d73b28c7a6218e1f3819df98b4b
Feng <fengyilei98@hotmail.com>
2020-01-09 14:27:46 -0500

    fix(toc): add config option to allow structured legends to delete layers

--- packages/ramp-core/src/app/geo/legend-block.class.js
+++ packages/ramp-core/src/app/geo/legend-block.class.js
@@ -437,11 +437,12 @@ function LegendBlockFactory(
         set metadataUrl(url) {
             this._layerConfig.metadataUrl = url;
         }
-
         get catalogueUrl() {
             return this._layerConfig.catalogueUrl;
         }
-
+        get enableStructuredDelete() {
+            return this._layerConfig.enableStructuredDelete;
+        }
         get availableControls() {
             return this._layerConfig.controls;
         }
@@ -798,6 +799,9 @@ function LegendBlockFactory(
             return [this.proxyWrapper].concat(this._controlledProxyWrappers);
         }
 
+        get enableStructuredDelete() {
+            return this.proxyWrapper.enableStructuredDelete;
+        }
         get availableControls() {
             return this.proxyWrapper.availableControls;
         }
