5ffd2c199a182d5a7bb981a7b14766fa1e21dbea
James Rae <James.Rae@ec.gc.ca>
2023-01-13 15:21:49 -0500

    chore(guts): remove http matching

--- packages/ramp-core/src/app/core/config.service.js
+++ packages/ramp-core/src/app/core/config.service.js
@@ -359,7 +359,7 @@ function configService($q, $rootElement, $http, $translate, events, gapiService,
 
         // load first config, other configs will be loaded as needed
         configList[0].promise.then((config) => {
-            let dojoUrl = (location.protocol === 'https:' ? 'https:' : 'http:') + '//js.arcgis.com/3.41/init.js';
+            let dojoUrl = 'https://js.arcgis.com/3.41/init.js';
             // initialize gapi and store a return promise
             if (typeof config.services._esriLibUrl !== 'undefined' && config.services._esriLibUrl !== '') {
                 dojoUrl = config.services._esriLibUrl;
